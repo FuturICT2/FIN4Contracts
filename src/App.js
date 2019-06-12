c689a6e813e4b858a9bb0769d202028a513993a3
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-12 14:29:33 +0200

    Create simple form

--- src/App.js
+++ src/App.js
@@ -2,20 +2,43 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  //constructor(props) {
-  //  super(props)
-  //}
+  constructor(props) {
+    super(props)
 
-  devFunc() {
-    console.log("dev");
+    this.state = {
+      name: '',
+      date: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={(e) => this.devFunc(e)}>
-          dev
-        </button>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+          </label>
+          <label>
+            Date:
+            <input name="date" type="date" value={this.state.date} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
