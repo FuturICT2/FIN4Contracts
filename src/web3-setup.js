245ad8d7d1301dc705da72dbd78edb827e18dfb2
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-14 09:22:27 +0200

    Add TODO to check out Drizzle

--- src/web3-setup.js
+++ src/web3-setup.js
@@ -11,6 +11,11 @@ let fin4BaseTokenAddress = '0x5D070A9E0eEC1ed26De488759C0Be9300c1107c6';
 let proofDummyAddress = '0x1DA05D2D4BAB428030558EeA4827dB61A80BA828';
 // let myAddress = web3.eth.getAccounts().then(a => a[0])
 
+// TODO use Drizzle? Seems to be useful, seven so I don't quite understand what it's used for yet :) 
+// https://github.com/trufflesuite/drizzle-react
+// The rental-nomad project used it too:
+// https://git.fortiss.org/Blockchain/student-practical-courses/blc4pi-ss2018/rental-nomad/blob/master/src/drizzleOptions.js
+
 const fin4BaseTokenContract = new web3.eth.Contract(fin4BaseTokenABI, fin4BaseTokenAddress);
 const proofDummyContract = new web3.eth.Contract(proofDummyABI, proofDummyAddress);
 
