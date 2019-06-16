1dc0411bac5413dc14a08c8d23d6870f78f602b9
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-16 22:05:58 +0200

    Make it work with drizzle
    Adopted mostly from the drizzle-box example: truffle unbox drizzle (https://github.com/truffle-box/drizzle-box)

--- src/App.js
+++ src/App.js
@@ -1,79 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
-import {fin4BaseTokenContract, proofDummyContract} from './web3-setup';
-//const BigchainDB = require('bigchaindb-driver')
-//const bip39 = require('bip39')
+import {DrizzleProvider} from "drizzle-react";
+import {LoadingContainer} from "drizzle-react-components";
+
+import drizzleOptions from "./drizzleOptions";
+import MyContainer from "./MyContainer";
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.handleSubmitClaim = this.submitClaim.bind(this);
-  }
-  
-  submitClaim() {
-    fin4BaseTokenContract.methods.submitClaim().send({
-      from: '0xB648892c8d3c1f5d38A3A7751cdc644Fd879Add1' // got from calling web3.eth.getAccounts()
-    });
-    // proofDummyContract.submitProof('0xEa3abc42dE134f0F2050d5D58b714f91bCe1CB0A', '0');
-  }
-
-  handleSubmit(event) {
-    console.log(this.state);
-    this.bigchainDBdev();
-    event.preventDefault();
-  }
-
   render() {
     return (
-      <div className="App">
-        <div>
-          <button onClick={this.handleSubmitClaim}>Submit a claim</button>
-        </div>
-      </div>
+      <DrizzleProvider options={drizzleOptions}>
+        <LoadingContainer>
+          <MyContainer />
+        </LoadingContainer>
+      </DrizzleProvider>
     );
   }
-
-  /*bigchainDBdev() {
-    // BigchainDB, wip following https://www.bigchaindb.com/developers/guide/tutorial-piece-of-art/
-    // see also here for more (better?) example: https://git.fortiss.org/Blockchain/student-practical-courses/blockchain-dev-env/blob/master/demos/bcdb-nodejs/demo.js
-    // requires a local BigchainDB running, either like this: https://blog.bigchaindb.com/the-status-of-the-bigchaindb-testnet-90d446edd2b4
-    // or like here: https://git.fortiss.org/Blockchain/student-practical-courses/blockchain-dev-env
-
-    const conn = new BigchainDB.Connection('http://localhost:9984/api/v1/') // https://test.bigchaindb.com/api/v1/
-    var seed = bip39.mnemonicToSeedSync("fantasticFin4 seedPhrase").slice(0, 32)
-    const alice = new BigchainDB.Ed25519Keypair(seed)
-
-    console.log(conn, seed, alice);
-
-    const painting = {
-      name: 'Meninas',
-      author: 'Diego Rodríguez de Silva y Velázquez',
-      place: 'Madrid',
-      year: '1656'
-    }
-    const txCreatePaint = BigchainDB.Transaction.makeCreateTransaction(
-        {
-            painting,
-        },
-        {
-            datetime: new Date().toString(),
-            location: 'Madrid',
-            value: {
-                value_eur: '25000000€',
-                value_btc: '2200',
-            }
-        },
-        [BigchainDB.Transaction.makeOutput(
-            BigchainDB.Transaction.makeEd25519Condition(alice.publicKey))],
-        alice.publicKey
-    )
-    const txSigned = BigchainDB.Transaction.signTransaction(txCreatePaint, alice.privateKey)
-    conn.postTransactionCommit(txSigned)
-        .then(res => {
-            document.body.innerHTML += '<h3>Transaction created</h3>';
-            document.body.innerHTML += txSigned.id
-        })
-  }*/
 }
 
 export default App;
---
+++ src/MyContainer.js
@@ -0,0 +1,15 @@
+import MyComponent from "./MyComponent";
+import {drizzleConnect} from "drizzle-react";
+
+const mapStateToProps = state => {
+  return {
+    accounts: state.accounts,
+    Fin4BaseToken: state.contracts.Fin4BaseToken,
+    ProofDummy: state.contracts.ProofDummy,
+    drizzleStatus: state.drizzleStatus,
+  };
+};
+
+const Container = drizzleConnect(MyComponent, mapStateToProps);
+
+export default Container;
