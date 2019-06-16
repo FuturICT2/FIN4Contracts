fa0dd05c44d6653353593d32ca20d09ff21696ae
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-16 21:51:27 +0200

    Cosmetic change: slash not necessary

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "/src/build/contracts"), // via https://ethereum.stackexchange.com/a/60575
+  contracts_build_directory: path.join(__dirname, "src/build/contracts"), // via https://ethereum.stackexchange.com/a/60575
   networks: {
     development: {
       host: "127.0.0.1",
