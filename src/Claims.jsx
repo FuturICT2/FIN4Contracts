2e4ae4392e57c0349df117638f15e7800281edcb
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-17 15:08:34 +0200

    Add links for figuring out how to handle multiple return arrays

--- src/Claims.jsx
+++ src/Claims.jsx
@@ -21,4 +21,9 @@ const Claims = () => (
 	</>
 );
 
+// TODO How to handle SC-methods that return multiple arrays? Either try getting this to work:
+// https://spectrum.chat/trufflesuite/drizzle/how-to-access-arrays-and-maps~1d591c68-42f0-4ca3-9f7f-10468446dfae
+// Or write our own element like "MultipleArrayContractData"?
+// https://github.com/trufflesuite/drizzle-react-components/blob/develop/src/ContractData.js
+
 export default Claims;
