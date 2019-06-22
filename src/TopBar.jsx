fd55847243f2220eb81918f75d39bd4c71fdf1d4
Simon <zachau@in.tum.de>
2019-06-22 21:13:54 +0200

    added header image

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -2,23 +2,16 @@ import React from 'react';
 import colors from './config/colors-config';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
 	bar: {
 		background: colors.main,
-		marginBottom: 20
+		marginBottom: 20,
 	},
-	menuButton: {
-		marginRight: theme.spacing(2)
-	},
-	title: {
-		flexGrow: 1,
-		textAlign: 'center',
-		fontSize: '2rem',
-		letterSpacing: 3,
-		fontWeight: 'bold'
+	headerImage: {
+		maxHeight: 50,
+		width: 300,
+		margin: '10px auto'
 	}
 }));
 
@@ -26,11 +19,7 @@ const TopBar = () => {
 	const classes = useStyles();
 	return (
 		<AppBar position="static" className={classes.bar}>
-			<Toolbar>
-				<Typography variant="h1" className={classes.title}>
-					FINFOO
-					</Typography>
-			</Toolbar>
+			<img src="/header.png" alt="header image of finfoo" className={classes.headerImage} />
 		</AppBar>
 	);
 };
