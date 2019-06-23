b9c54fb636e549d200743eff603e4ec468a87c34
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-23 02:39:49 +0200

    Create ProofDummy proof type sc

--- src/config/drizzle-config.js
+++ src/config/drizzle-config.js
@@ -1,5 +1,6 @@
 import Fin4Main from '../build/contracts/Fin4Main.json';
 import Fin4Claim from '../build/contracts/Fin4Claim.json';
+import ProofDummy from '../build/contracts/ProofDummy.json';
 
 const drizzleConfig = {
 	web3: {
@@ -9,7 +10,7 @@ const drizzleConfig = {
 			url: 'ws://127.0.0.1:7545'
 		}
 	},
-	contracts: [Fin4Main, Fin4Claim]
+	contracts: [Fin4Main, Fin4Claim, ProofDummy]
 };
 
 export default drizzleConfig;
