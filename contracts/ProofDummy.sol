b2fe326c2af1d0a7b3034d8d8b21b1158070ea4d
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-23 02:42:12 +0200

    Fix typo

--- contracts/ProofDummy.sol
+++ contracts/ProofDummy.sol
@@ -5,7 +5,7 @@ import "contracts/Fin4Token.sol";
 contract ProofDummy {
 
   function submitProof(address SCtoReceiveProof, uint claimId) public returns(bool) {
-    Fin4BaseToken(SCtoReceiveProof).receiveApprovedProof(msg.sender, claimId);
+    Fin4Token(SCtoReceiveProof).receiveApprovedProof(msg.sender, claimId);
     return true;
   }
 
