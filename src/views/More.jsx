6436f62686cc6175763510ff1f316d68557c22cf
Simon <zachau@in.tum.de>
2019-06-23 07:13:44 +0200

    donation UI

--- src/views/More.jsx
+++ src/views/More.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import DonorCard from '../DonorCard';
 // import { LoadingContainer } from 'drizzle-react-components';
 // import ContractData from '../ContractData';
 
-const Home = () => (
-	<p>TODO: record view</p>
-);
+const donationReceivers = [
+	{
+		title: "WWF",
+		imagePath: "http://tous-logos.com/wp-content/uploads/2017/03/WWF-logo-400x333.png",
+		description: "The leading organization in wildlife conservation and endangered species.",
+		readMore: "https://www.wwf.ch/de"
+	},
+	{
+		title: "Unicef",
+		imagePath: "https://www.psi.org/wp-content/uploads/2014/10/UNICEF-150x150.jpeg",
+		description: "UNICEF works across 190 countries and territories to reach the most disadvantaged children and adolescents.",
+		readMore: "https://www.unicef.ch/en"
+	}
+];
 
-export default Home;
+class More extends React.Component {
+	render() {
+		return (
+			<div>
+				{donationReceivers.map(d => {
+					return <DonorCard
+						title={d.title}
+						imagePath={d.imagePath}
+						description={d.description}
+						readMore={d.readMore}
+					/>
+				})}
+			</div>
+		)
+	}
+}
+
+export default More;
