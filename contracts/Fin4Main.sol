4c8181f3acdc760b9cd721fe419473741d9b31eb
Sangeeta Joseph <sangeeta.joseph@elasticbrains.de>
2019-06-23 06:41:49 +0200

    Added a new quantity column for claims table and added the logic to mint token

--- contracts/Fin4Main.sol
+++ contracts/Fin4Main.sol
@@ -92,12 +92,23 @@ contract Fin4Main {
     for (uint i = 0; i < requiredProofs.length; i ++) {
       claim.proof_statuses[requiredProofs[i]] = false;
     }
-    claim.isApproved = false;
+
+    //TODO: Need to be always set to false
+    //We set it to true just for the Demo to show the transfer to token to the user
+    if(nextClaimId % 2 == 0){
+        claim.isApproved = false;
+    }else{
+      claim.isApproved = true;
+      mintToken(action, quantity);
+    }
+    
     nextClaimId ++;
     return nextClaimId - 1;
+
+
   }
 
-  function getStatuses() public view returns(uint[] memory, bool[] memory) {
+  function getStatuses() public view returns(uint[] memory, bool[] memory, uint[] memory) {
     uint count = 0;
     for (uint i = 0; i < nextClaimId; i ++) {
       if (claims[i].claimer == msg.sender) {
@@ -105,16 +116,18 @@ contract Fin4Main {
       }
     }
     uint[] memory ids = new uint[](count);
+    uint[] memory quantity = new uint[](count);
     bool[] memory states = new bool[](count);
     count = 0;
     for (uint i = 0; i < nextClaimId; i ++) {
       if (claims[i].claimer == msg.sender) {
           ids[count] = i;
           states[count] = claims[i].isApproved;
+          quantity[count] = claims[i].quantity;
           count ++;
       }
     }
-    return (ids,states);
+    return (ids,states,quantity);
   }
 
   // for dev purposes only, this is NOT the normal flow
