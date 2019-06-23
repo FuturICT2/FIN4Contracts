eede4fe881583b25db466fd49e3ac76d7ddd8077
Simon <zachau@in.tum.de>
2019-06-23 10:07:12 +0200

    market styles

--- src/views/More.jsx
+++ src/views/More.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { LoadingContainer } from 'drizzle-react-components';
 import Container from '../Styles';
 import ContractRetriever from '../ContractRetriever';
+import styled from 'styled-components';
 
 import DonorCard from '../DonorCard';
 // import { LoadingContainer } from 'drizzle-react-components';
@@ -25,7 +26,7 @@ const donationReceivers = [
 class More extends React.Component {
 	render() {
 		return (
-			<>
+			<Wrapper>
 				<Container>
 					<LoadingContainer>
 						<ContractRetriever title="My Action Tokens" />
@@ -42,9 +43,15 @@ class More extends React.Component {
 						/>
 					})}
 				</div>
-			</>
+			</Wrapper>
 		)
 	}
 }
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+`
 
 export default More;
