f6fecc4b754852154f3632507c105faed53d8cc9
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-26 00:53:38 +0200

    Proof types are better located in the Claim SC

--- contracts/Fin4Token.sol
+++ contracts/Fin4Token.sol
@@ -3,19 +3,21 @@ pragma solidity ^0.5.0;
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
 
-import 'contracts/Fin4MainAbstract.sol';
+import 'contracts/Fin4ClaimAbstract.sol';
 
 contract Fin4Token is ERC20Detailed, ERC20Mintable {
 
-  constructor(string memory name, string memory symbol, uint8 decimals, address Fin4MainAdr)
+  constructor(string memory name, string memory symbol, uint8 decimals, address Fin4MainAdr, address Fin4ClaimAdr)
     ERC20Detailed(name, symbol, decimals)
     ERC20Mintable()
     ERC20()
     public {
       Fin4Main_adr = Fin4MainAdr;
+      Fin4Claim_adr = Fin4ClaimAdr;
     }
 
   address public Fin4Main_adr;
+  address public Fin4Claim_adr;
   address[] public requiredProofTypes;
 
   // called from ProofDummy, therefore msg.sender is the address of that SC
@@ -30,8 +32,8 @@ contract Fin4Token is ERC20Detailed, ERC20Mintable {
   }
 
   function addRequiredProofType(address proofType) public returns(bool) {
-    // bool isRegistered = Fin4Main_adr.call(bytes4(sha3("proofTypeIsRegistered(address)")), proofType);
-    require(Fin4MainAbstract(Fin4Main_adr).proofTypeIsRegistered(proofType), "Proof type is not registered in Fin4Main");
+    // bool isRegistered = Fin4Claim_adr.call(bytes4(sha3("proofTypeIsRegistered(address)")), proofType);
+    require(Fin4ClaimAbstract(Fin4Claim_adr).proofTypeIsRegistered(proofType), "Proof type is not registered in Fin4Main");
     requiredProofTypes.push(proofType);
     return true;
   }
