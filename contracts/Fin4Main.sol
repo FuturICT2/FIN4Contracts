7205439fb9513e1fa1447b3651bd356bf2ab2ff4
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-27 16:33:45 +0200

    Remove claim logic from Fin4Main

--- contracts/Fin4Main.sol
+++ contracts/Fin4Main.sol
@@ -17,7 +17,7 @@ contract Fin4Main {
     return children;
   }
 
-  function transferTokens(address tokenAddress, address accountAddress) public{
+  function transferTokens(address tokenAddress, address accountAddress) public {
       Fin4Token token = Fin4Token(tokenAddress);
       token.transfer(accountAddress,1);
   }
@@ -31,12 +31,12 @@ contract Fin4Main {
     return children;
   }
 
-  function mintToken(address tokenAddress,uint256 amount) public{
+  function mintToken(address tokenAddress,uint256 amount) public {
       Fin4Token token = Fin4Token(tokenAddress);
       token.mint(msg.sender,amount);
   }
 
-  function getBalance(address tokenAddress) public view returns(uint256){
+  function getBalance(address tokenAddress) public view returns(uint256) {
       return Fin4Token(tokenAddress).balanceOf(msg.sender);
   }
 
@@ -64,71 +64,5 @@ contract Fin4Main {
     return (addresses, balances);
   }
 
-  //TODO: Mintable Role for other contracts trying to run the mintToken funtion.
-
-  uint nextClaimId = 0;
-
-	struct Claim {
-    uint claimId;
-    address claimer;
-    bool isApproved;
-    address actionAdr;
-    uint quantity;
-    uint date;
-    string comment;
-    mapping(address => bool) proof_statuses;
-  }
-
-	mapping (uint => Claim) public claims;
-
-	function submit(address action, uint quantity, uint date, string memory comment) public returns (uint) {
-    Claim storage claim = claims[nextClaimId];
-    claim.claimer = msg.sender;
-    claim.actionAdr = action;
-    claim.quantity = quantity;
-    claim.date = date;
-    claim.comment = comment;
-    address[] memory requiredProofs = Fin4Token(action).getRequiredProofTypes();
-    for (uint i = 0; i < requiredProofs.length; i ++) {
-      claim.proof_statuses[requiredProofs[i]] = false;
-    }
-    //TODO: Need to be always set to false
-    //We set it to true just for the Demo to show the transfer to token to the user
-    if (nextClaimId % 2 == 0) {
-        claim.isApproved = false;
-    } else {
-      claim.isApproved = true;
-      mintToken(action, quantity);
-    }
-    nextClaimId ++;
-    return nextClaimId - 1;
-  }
-
-  function getStatuses() public view returns(uint[] memory, bool[] memory, uint[] memory) {
-    uint count = 0;
-    for (uint i = 0; i < nextClaimId; i ++) {
-      if (claims[i].claimer == msg.sender) {
-          count ++;
-      }
-    }
-    uint[] memory ids = new uint[](count);
-    uint[] memory quantity = new uint[](count);
-    bool[] memory states = new bool[](count);
-    count = 0;
-    for (uint i = 0; i < nextClaimId; i ++) {
-      if (claims[i].claimer == msg.sender) {
-          ids[count] = i;
-          states[count] = claims[i].isApproved;
-          quantity[count] = claims[i].quantity;
-          count ++;
-      }
-    }
-    return (ids,states,quantity);
-  }
-
-  // for dev purposes only, this is NOT the normal flow
-  function approveClaim(uint claimId) public returns(bool) {
-    claims[claimId].isApproved = true;
-    return true;
-  }
+  // TODO: Mintable Role for other contracts trying to run the mintToken funtion.
 }
