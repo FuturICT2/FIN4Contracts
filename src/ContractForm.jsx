ad0ecc1aa10c856e7204a54e15eac85c912a2361
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-30 00:43:19 +0200

    ContractForm also needs to support more contracts

--- src/ContractForm.jsx
+++ src/ContractForm.jsx
@@ -43,12 +43,18 @@ class ContractForm extends Component {
 
 		if (this.props.contractAddress) {
 			const web3 = new Web3(window.web3.currentProvider);
-			var Fin4TokenJson = require('./build/contracts/Fin4Token.json');
+
+			var contractJson = 'Fin4Token.json';
+			if (this.props.contractJson) {
+				contractJson = this.props.contractJson;
+			}
+
+			var tokenJson = require('./build/contracts/Fin4Token.json');
 
 			// needs time and has no callback -> timout below
 			this.context.drizzle.addContract({
 				contractName: this.props.contractAddress,
-				web3Contract: new web3.eth.Contract(Fin4TokenJson.abi, this.props.contractAddress)
+				web3Contract: new web3.eth.Contract(tokenJson.abi, this.props.contractAddress)
 			});
 
 			this.contractIdentifier = this.props.contractAddress;
