8624ff97cadece0d3f1655a11d1159a31acedd16
Simon <zachau@in.tum.de>
2019-06-30 01:39:26 +0200

    clean up ContractRetriever and ProofSubmission

--- src/views/ProofSubmission.jsx
+++ src/views/ProofSubmission.jsx
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import Container from '../Styles';
 
 class ProofSubmission extends Component {
-	constructor(props, context) {
+	constructor(props) {
 		super(props);
-        this.state = {};
+		this.state = {};
 
-        // TODO make this parsing more robust with errors if wrong etc.
-        var url = window.location.href;
-        var params = url.split('?')[1].split('&');
-        this.tokenAddress = params[0].split('=')[1];
-        this.claimId = Number(params[1].split('=')[1]);
-    }
+		// TODO make this parsing more robust with errors if wrong etc.
+		var url = window.location.href;
+		var params = url.split('?')[1].split('&');
+		this.tokenAddress = params[0].split('=')[1];
+		this.claimId = Number(params[1].split('=')[1]);
+	}
 
 	render() {
 		return (
 			<Container>
-                {this.tokenAddress} {this.claimId}
+				{this.tokenAddress} {this.claimId}
 			</Container>
-		)
+		);
 	}
 }
 
