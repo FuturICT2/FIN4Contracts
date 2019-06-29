12a64d9d592f63f66dd43c464b5b8cf821ce7872
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-29 16:07:10 +0200

    Link ProofTypes to Fin4Main already during deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,25 @@
 const Fin4Main = artifacts.require('Fin4Main');
-const ProofDummy = artifacts.require('ProofDummy');
+const ImmediateAutoApproval = artifacts.require('ImmediateAutoApproval');
+const ApprovalByTokenCreator = artifacts.require('ApprovalByTokenCreator');
 
-module.exports = function (deployer) {
-	deployer.deploy(Fin4Main);
-	deployer.deploy(ProofDummy);
+module.exports = async function (deployer) {
+
+	// via https://ethereum.stackexchange.com/a/30579
+
+	await Promise.all([
+		deployer.deploy(ImmediateAutoApproval),
+		deployer.deploy(ApprovalByTokenCreator),
+		deployer.deploy(Fin4Main)
+	  ]);
+
+	var instances = await Promise.all([
+		ImmediateAutoApproval.deployed(),
+		ApprovalByTokenCreator.deployed(),
+		Fin4Main.deployed()
+	])
+
+	await Promise.all([
+		instances[2].addProofType(instances[0].address),
+		instances[2].addProofType(instances[1].address),
+	]);
 };
