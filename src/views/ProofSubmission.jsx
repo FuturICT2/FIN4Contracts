3c19131dd73c914feff090d2cccfda15baab70d4
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-06-29 22:56:08 +0200

    Display getClaim on /proof site

--- src/views/ProofSubmission.jsx
+++ src/views/ProofSubmission.jsx
@@ -2,20 +2,6 @@ import React, { Component } from 'react';
 import Container from '../Styles';
 import ContractData from '../ContractData';
 
-const requiredProofTypeAddresses = data => {
-    var tokenName = data[0];
-    var tokenSymbol = data[1];
-    var claimer = data[2];
-    var isApproved = data[3];
-    var quantity = Number(data[4]);
-    var date = Number(data[5]);
-    var comment = data[6];
-    var requiredProofTypes = data[7];
-    var proofTypeStatuses = data[8];
-
-	return "";
-}
-
 class ProofSubmission extends Component {
 	constructor(props, context) {
 		super(props);
@@ -28,15 +14,37 @@ class ProofSubmission extends Component {
         this.claimId = Number(params[1].split('=')[1]);
     }
 
+    requiredProofTypeAddresses = data => {
+        var tokenName = data[0];
+        var tokenSymbol = data[1];
+        var claimer = data[2];
+        var isApproved = data[3];
+        var quantity = Number(data[4]);
+        var date = Number(data[5]);
+        var comment = data[6];
+        var requiredProofTypes = data[7];
+        var proofTypeStatuses = data[8];
+
+        const proofTypes = requiredProofTypes.map((address, index) => {
+            return (
+                <span key={index}><hr></hr>{address}<br></br>{proofTypeStatuses[index] + ""}<br></br><br></br></span>
+            );
+        });
+
+        return (<div>Claim <i>{this.claimId}</i> on action type <b>{tokenName}</b> [{tokenSymbol}]<br></br><br></br>
+        isApproved: <i>{isApproved + ""}</i>, quantity: <i>{quantity}</i>, date: <i>{date}</i>, comment: <i>{comment}</i><br></br><br></br>
+        {proofTypes}
+        </div>);
+    }
+
 	render() {
 		return (
 			<Container>
-                {this.tokenAddress} {this.claimId}
                 <ContractData
                     contractAddress={this.tokenAddress}
                     method="getClaim"
                     methodArgs={[this.claimId]}
-					callback={requiredProofTypeAddresses}
+					callback={this.requiredProofTypeAddresses}
 				/>
 			</Container>
 		)
