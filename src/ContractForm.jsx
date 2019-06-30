558a18e57a782432695093b0c48dd48a20d375a3
Simon <zachau@in.tum.de>
2019-06-30 12:36:28 +0200

    comment drizzle components

--- src/ContractForm.jsx
+++ src/ContractForm.jsx
@@ -1,6 +1,7 @@
+// heavily adapted from https://github.com/trufflesuite/drizzle-react-components/blob/develop/src/ContractForm.js
+
 import { drizzleConnect } from 'drizzle-react';
 import React, { Component } from 'react';
-
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
