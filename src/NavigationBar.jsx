0f8aca4b4807a9f3366c0b3e05769d16a2ae379d
Simon <zachau@in.tum.de>
2019-06-30 04:12:05 +0200

    menu item in navigation bar highlights correctly on direct call

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,52 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import colors from './config/colors-config';
 import menuItems from './config/menu-config';
-import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import styled from 'styled-components';
 
-const useStyles = makeStyles({
-	root: {
-		background: colors.main,
-		position: 'fixed',
-		bottom: 0,
-		left: 0,
-		right: 0
-	},
-	element: {
-		color: colors.main2,
-		maxWidth: 'none',
-		width: '100%'
+class NavigationBar extends Component {
+	constructor(props) {
+		super(props);
+		// get currently active url... if it doesnt't find it in the menu, it's -1.
+		const activeElement = menuItems
+			.map(menuItem => menuItem.path)
+			.indexOf(window.location.pathname);
+		this.state = {
+			value: Math.max(0, activeElement)
+		};
 	}
-});
 
-const NavigationBar = function () {
-	const classes = useStyles();
-	const [value, setValue] = React.useState(0);
+	render() {
+		return (
+			<MenuContainer
+				value={this.state.value}
+				onChange={(event, newValue) => {
+					this.setState({ value: newValue });
+				}}
+				showLabels>
+				{menuItems.map((route, i) => {
+					return (
+						<MenuItem
+							key={i}
+							component={Link}
+							to={route.path}
+							label={route.label}
+							icon={<route.icon />}
+						/>
+					);
+				})}
+			</MenuContainer>
+		);
+	}
+}
 
-	return (
-		<BottomNavigation
-			value={value}
-			onChange={(event, newValue) => {
-				setValue(newValue);
-			}}
-			showLabels
-			className={classes.root}>
-			{menuItems.map((route, i) => {
-				return (
-					<BottomNavigationAction
-						key={i}
-						component={Link}
-						to={route.path}
-						label={route.label}
-						icon={<route.icon />}
-						className={classes.element}
-					/>
-				);
-			})}
-		</BottomNavigation>
-	);
-};
+const MenuContainer = styled(BottomNavigation)`
+	&& {
+		background: ${colors.main};
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+	}
+`;
+
+const MenuItem = styled(BottomNavigationAction)`
+	&& {
+		color: ${colors.main2};
+		maxwidth: none;
+		width: 100%;
+	}
+`;
 
 export default NavigationBar;
