e0bd093084f42526d3a13268b72913e6ec4a1fda
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-07-01 22:35:25 +0200

    Move ProofType things to Fin4TokenBase

--- contracts/Fin4Token.sol
+++ contracts/Fin4Token.sol
@@ -4,8 +4,6 @@ import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
 
 import 'contracts/tokens/ERC20Mintable.sol';
 import 'contracts/Fin4TokenBase.sol';
-import 'contracts/Fin4MainStrut.sol';
-import "contracts/proof/Fin4BaseProofType.sol";
 
 contract Fin4Token is Fin4TokenBase, ERC20Detailed, ERC20Mintable {
 
@@ -16,61 +14,8 @@ contract Fin4Token is Fin4TokenBase, ERC20Detailed, ERC20Mintable {
     Fin4TokenBase(Fin4MainAddress, tokenCreatorAddress)
     public {}
 
-  function getName() public view returns(string memory) {
-    return name();
-  }
-
-  function getSymbol() public view returns(string memory) {
-    return symbol();
-  }
-
-  function getInfo() public view returns(string memory, string memory) {
-    return (name(), symbol());
-  }
-
-  address[] public requiredProofTypes;
-
-  function pingbackClaimSubmissionToMain() public returns(bool) {
-    Fin4MainStrut(Fin4Main).claimSubmissionPingback(msg.sender);
-    return true;
-  }
-
-  // called from ProofType contracts, therefore msg.sender is the address of that SC
-  function receiveProofApproval(address claimer, uint claimId) public returns(bool) {
-    claims[claimId].proof_statuses[msg.sender] = true;
-    if (_allProofTypesApprovedOnClaim(claimId)) {
-      claims[claimId].isApproved = true;
-      // TODO mint
-    }
-    return true;
-  }
-
-  function _allProofTypesApprovedOnClaim(uint claimId) private view returns(bool) {
-    for (uint i = 0; i < requiredProofTypes.length; i ++) {
-      if (!claims[claimId].proof_statuses[requiredProofTypes[i]]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function getRequiredProofTypes() public view returns(address[] memory) {
-    return requiredProofTypes;
-  }
-
-  function addRequiredProofType(address proofType) public returns(bool) {
-    // bool isRegistered = Fin4Main.call(bytes4(sha3("proofTypeIsRegistered(address)")), proofType);
-    require(Fin4MainStrut(Fin4Main).proofTypeIsRegistered(proofType), "This address is not registered as proof type in Fin4Main");
-    requiredProofTypes.push(proofType);
-    Fin4BaseProofType(proofType).registerTokenUsingThisProofType(address(this));
-    return true;
-  }
-
-  function balanceOfMe() public view returns(uint256) {
-    return balanceOf(msg.sender);
-  }
-
   function getInfoAndBalance() public view returns(string memory, string memory, uint256) {
     return (name(), symbol(), balanceOf(msg.sender));
   }
+
 }
--- contracts/Fin4TokenBase.sol
+++ contracts/Fin4TokenBase.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+import 'contracts/Fin4MainStrut.sol';
+import "contracts/proof/Fin4BaseProofType.sol";
+
 contract Fin4TokenBase { // abstract class
 
   address public Fin4Main;
@@ -10,6 +13,15 @@ contract Fin4TokenBase { // abstract class
     Fin4Main = Fin4MainAddress;
     tokenCreator = tokenCreatorAddress;
   }
+
+  function name() public view returns(string memory);
+  function symbol() public view returns(string memory);
+
+  function getInfo() public view returns(string memory, string memory) {
+    return (name(), symbol());
+  }
+
+  // ------------------------- CLAIM -------------------------
 
   uint nextClaimId = 0;
 
@@ -48,30 +60,26 @@ contract Fin4TokenBase { // abstract class
     return nextClaimId - 1;
   }
 
-  function getName() public view returns(string memory);
-
-  function getSymbol() public view returns(string memory);
-
-  function pingbackClaimSubmissionToMain() public returns(bool);
-
-  function getRequiredProofTypes() public view returns(address[] memory);
-
-  function getClaim(uint claimId) public view returns(string memory, string memory, address, bool, uint, uint, string memory, address[] memory, bool[] memory) {
+  function getClaim(uint claimId) public view returns(string memory, string memory,
+    address, bool, uint, uint, string memory, address[] memory, bool[] memory) {
     // require(claims[claimId].claimer == msg.sender, "This claim was not submitted by the sender");
 
     Claim storage claim = claims[claimId];
-    // this assumes these are still the same as when the claim was submitted, should we support an evolving set of proof types though? TODO
+    // this assumes these are still the same as when the claim was submitted
+    // should we support an evolving set of proof types though? TODO
     address[] memory requiredProofTypes = getRequiredProofTypes();
     bool[] memory proofTypeStatuses = new bool[](requiredProofTypes.length);
     for (uint i = 0; i < requiredProofTypes.length; i ++) {
       proofTypeStatuses[i] = claim.proof_statuses[requiredProofTypes[i]];
     }
 
-    return (getName(), getSymbol(), claim.claimer, claim.isApproved, claim.quantity, claim.date, claim.comment, requiredProofTypes, proofTypeStatuses);
+    return (name(), symbol(), claim.claimer, claim.isApproved,
+      claim.quantity, claim.date, claim.comment, requiredProofTypes, proofTypeStatuses);
   }
 
   function getClaimInfo(uint claimId) public view returns(address, bool, uint, uint, string memory) {
-    return (claims[claimId].claimer, claims[claimId].isApproved, claims[claimId].quantity, claims[claimId].date, claims[claimId].comment);
+    return (claims[claimId].claimer, claims[claimId].isApproved,
+      claims[claimId].quantity, claims[claimId].date, claims[claimId].comment);
   }
 
   function getMyClaimIds() public view returns(address, string memory, string memory, uint[] memory) {
@@ -89,7 +97,48 @@ contract Fin4TokenBase { // abstract class
           count ++;
       }
     }
-    return (address(this), getName(), getSymbol(), ids);
+    return (address(this), name(), symbol(), ids);
+  }
+
+  // ------------------------- PROOF TYPES -------------------------
+
+  address[] public requiredProofTypes;
+
+  function pingbackClaimSubmissionToMain() public returns(bool) {
+    Fin4MainStrut(Fin4Main).claimSubmissionPingback(msg.sender);
+    return true;
+  }
+
+  // called from ProofType contracts, therefore msg.sender is the address of that SC
+  function receiveProofApproval(address claimer, uint claimId) public returns(bool) {
+    claims[claimId].proof_statuses[msg.sender] = true;
+    if (_allProofTypesApprovedOnClaim(claimId)) {
+      claims[claimId].isApproved = true;
+      // TODO mint
+    }
+    return true;
+  }
+
+  function _allProofTypesApprovedOnClaim(uint claimId) private view returns(bool) {
+    for (uint i = 0; i < requiredProofTypes.length; i ++) {
+      if (!claims[claimId].proof_statuses[requiredProofTypes[i]]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function getRequiredProofTypes() public view returns(address[] memory) {
+    return requiredProofTypes;
+  }
+
+  function addRequiredProofType(address proofType) public returns(bool) {
+    // bool isRegistered = Fin4Main.call(bytes4(sha3("proofTypeIsRegistered(address)")), proofType);
+    require(Fin4MainStrut(Fin4Main).proofTypeIsRegistered(proofType),
+      "This address is not registered as proof type in Fin4Main");
+    requiredProofTypes.push(proofType);
+    Fin4BaseProofType(proofType).registerTokenUsingThisProofType(address(this));
+    return true;
   }
 
 }
