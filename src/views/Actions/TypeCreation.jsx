835302f90ebda857af03883ef2f5be75de3957a8
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-07-02 15:58:19 +0200

    Experimental: direct call to contract, no drizzle-cache-call
    @simonzachau said we'll need this to get the dropdown/search/multiselect components working

--- src/views/Actions/TypeCreation.jsx
+++ src/views/Actions/TypeCreation.jsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import ContractForm from '../../ContractForm';
 import ContractData from '../../ContractData';
 import { Box } from '../../Styles';
+import { drizzleConnect } from 'drizzle-react';
+import PropTypes from 'prop-types';
 
 class TypeCreation extends Component {
+	constructor(props, context) {
+		super(props);
+		// For more stages other than "then", see https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html
+		context.drizzle.contracts.Fin4Main.methods.getProofTypes().call().then((data) => { 
+			console.log("ProofTypes on Fin4Main: ", data);
+		});
+	}
+
 	getProofTypes = data => {
 		const proofTypes =
 			data &&
@@ -40,4 +50,14 @@ class TypeCreation extends Component {
 	}
 }
 
-export default TypeCreation;
+TypeCreation.contextTypes = {
+	drizzle: PropTypes.object
+};
+
+const mapStateToProps = state => {
+	return {
+		contracts: state.contracts
+	};
+};
+
+export default drizzleConnect(TypeCreation, mapStateToProps);
