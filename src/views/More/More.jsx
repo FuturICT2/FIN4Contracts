3a460c3cd4a7a6ac93354708e3df254f5870fb30
Simon <zachau@in.tum.de>
2019-07-03 02:34:33 +0200

    apply Fin4Table to More tab

--- src/views/More/More.jsx
+++ src/views/More/More.jsx
@@ -1,50 +1,26 @@
 import React from 'react';
-import { Fin4Container, Fin4Card } from '../../Elements';
+import { Fin4Container, Fin4Card, Fin4Box, Fin4Table, Fin4TableRow } from '../../Elements';
 import ContractData from '../../ContractData';
 import styled from 'styled-components';
 import dummyData from '../../config/dummy-data';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-
-const showRow = ({ 0: name, 1: symbol, 2: balance }) => {
-	return (
-		<TableRow key={symbol}>
-			<TableCell key="1">{name}</TableCell>
-			<TableCell key="2">{symbol}</TableCell>
-			<TableCell key="3">{balance}</TableCell>
-		</TableRow>
-	);
-};
 
 const showBalanceByActionType = data => {
 	return (
-		<Paper>
-			<Typography variant="h5" component="h3">
-				My Action Tokens
-			</Typography>
-			<Table>
-				<TableHead>
-					<TableRow>
-						{['Name', 'Symbol', 'Balance'].map((key, index) => {
-							return <TableCell key={index}>{key}</TableCell>;
-						})}
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{data &&
-						data.map((address, index) => {
-							return (
-								<ContractData key={index} contractAddress={address} method="getInfoAndBalance" callback={showRow} />
-							);
-						})}
-				</TableBody>
-			</Table>
-		</Paper>
+		<Fin4Box title="My Action Tokens">
+			<Fin4Table headers={['Name', 'Symbol', 'Balance']}>
+				{data &&
+					data.map((address, index) => {
+						return (
+							<ContractData
+								key={index}
+								contractAddress={address}
+								method="getInfoAndBalance"
+								callback={data => <Fin4TableRow index={index} data={data} />}
+							/>
+						);
+					})}
+			</Fin4Table>
+		</Fin4Box>
 	);
 };
 
