15285af6690070db0df8fe0d84e1d1fa9a78e965
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-07-03 13:41:54 +0200

    Restore Messages.jsx to earlier commit
    To commit cdfe74b0b5ad841e957e964242afcbd42d1a2ae5, @simonzachau accidentally overwrote my changes when merging

--- src/views/Messages.jsx
+++ src/views/Messages.jsx
@@ -1,44 +1,51 @@
 import React, { Component } from 'react';
-import { Fin4Box } from '../Elements';
 import ContractData from '../ContractData';
+import { Fin4Box } from '../Elements';
 
 class Messages extends Component {
-	getMyMessage = ({ 0: sender, 1: message }) => {
-		return (
-			<p>
-				{message} <i>{sender}</i>
-			</p>
-		);
-	};
+
+    getMyMessage = ({ 0: sender, 1: message, 2: fulfillmentAddress, 3: proofTypeName }) => {
+        return <li>
+                <div>
+                    {message}<br></br>Requested by: <i>{sender}</i><br></br>
+                    <b><a href={`/approving?fulfillmentAddress=${fulfillmentAddress}&proofTypeName=${proofTypeName}`}>Go to approval page</a></b>
+                </div>
+            </li>
+    };
 
 	getMyMessagesCount = data => {
-		var messageCount = Number(data);
-		if (messageCount === 0) {
-			return <></>;
-		}
-		var messageIndices = [];
-		for (var i = 0; i < messageCount; i++) {
-			messageIndices.push(i);
-		}
-		return (
-			<Fin4Box>
-				{messageIndices.map(index => {
-					return (
-						<ContractData
-							key={index}
-							contractName="Fin4Main"
-							method="getMyMessage"
-							methodArgs={[index]}
-							callback={this.getMyMessage}
-						/>
-					);
-				})}
-			</Fin4Box>
-		);
+        var messageCount = Number(data);
+        if (messageCount === 0) {
+            return <i>No messages</i>;
+        }
+        var messageIndices = [];
+        for (var i = 0; i < messageCount; i++) {
+            messageIndices.push(i);
+        }
+		const messageItems =
+            messageIndices.map((index) => {
+                return (
+                <ContractData 
+                    key={index}
+                    contractName="Fin4Main"
+                    method="getMyMessage"
+                    methodArgs={[index]}
+                    callback={this.getMyMessage} 
+                />);
+			});
+		return <ul>{messageItems}</ul>;
 	};
 
 	render() {
-		return <ContractData contractName="Fin4Main" method="getMyMessagesCount" callback={this.getMyMessagesCount} />;
+		return (
+			<Fin4Box title="Messages">
+				<ContractData
+					contractName="Fin4Main"
+					method="getMyMessagesCount"
+					callback={this.getMyMessagesCount}
+				/>
+			</Fin4Box>
+		);
 	}
 }
 
