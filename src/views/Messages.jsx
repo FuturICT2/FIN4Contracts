137fdfcbd90d374d05ea3d2bbaaea1be67e2498c
Simon <zachau@in.tum.de>
2019-07-03 13:57:23 +0200

    #715b42f87704a67fb949c76ed034f5b125efeba6 because of overwrite #15285af6690070db0df8fe0d84e1d1fa9a78e965 by @benjaminaaron

--- src/views/Messages.jsx
+++ src/views/Messages.jsx
@@ -3,49 +3,51 @@ import ContractData from '../ContractData';
 import { Fin4Box } from '../Elements';
 
 class Messages extends Component {
-
-    getMyMessage = ({ 0: sender, 1: message, 2: fulfillmentAddress, 3: proofTypeName }) => {
-        return <li>
-                <div>
-                    {message}<br></br>Requested by: <i>{sender}</i><br></br>
-                    <b><a href={`/approving?fulfillmentAddress=${fulfillmentAddress}&proofTypeName=${proofTypeName}`}>Go to approval page</a></b>
-                </div>
-            </li>
-    };
+	getMyMessage = ({ 0: sender, 1: message, 2: fulfillmentAddress, 3: proofTypeName }) => {
+		return (
+			<li>
+				<div>
+					{message}
+					<br></br>Requested by: <i>{sender}</i>
+					<br></br>
+					<b>
+						<a href={`/approving?fulfillmentAddress=${fulfillmentAddress}&proofTypeName=${proofTypeName}`}>
+							Go to approval page
+						</a>
+					</b>
+				</div>
+			</li>
+		);
+	};
 
 	getMyMessagesCount = data => {
-        var messageCount = Number(data);
-        if (messageCount === 0) {
-            return <i>No messages</i>;
-        }
-        var messageIndices = [];
-        for (var i = 0; i < messageCount; i++) {
-            messageIndices.push(i);
-        }
-		const messageItems =
-            messageIndices.map((index) => {
-                return (
-                <ContractData 
-                    key={index}
-                    contractName="Fin4Main"
-                    method="getMyMessage"
-                    methodArgs={[index]}
-                    callback={this.getMyMessage} 
-                />);
-			});
-		return <ul>{messageItems}</ul>;
+		var messageCount = Number(data);
+		if (messageCount === 0) {
+			return <></>;
+		}
+		var messageIndices = [];
+		for (var i = 0; i < messageCount; i++) {
+			messageIndices.push(i);
+		}
+		return (
+			<Fin4Box title="Messages">
+				{messageIndices.map(index => {
+					return (
+						<ContractData
+							key={index}
+							contractName="Fin4Main"
+							method="getMyMessage"
+							methodArgs={[index]}
+							callback={this.getMyMessage}
+						/>
+					);
+				})}
+			</Fin4Box>
+		);
 	};
 
 	render() {
-		return (
-			<Fin4Box title="Messages">
-				<ContractData
-					contractName="Fin4Main"
-					method="getMyMessagesCount"
-					callback={this.getMyMessagesCount}
-				/>
-			</Fin4Box>
-		);
+		return <ContractData contractName="Fin4Main" method="getMyMessagesCount" callback={this.getMyMessagesCount} />;
 	}
 }
 
