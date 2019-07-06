b0520d6c89fc6c4e6b40140130b8e6e96fddf22a
Simon <zachau@in.tum.de>
2019-07-06 23:45:30 +0200

    simplify migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,38 +6,28 @@ const MinimumClaimingInterval = artifacts.require('MinimumClaimingInterval');
 const Password = artifacts.require('Password');
 const MaximumQuantityPerInterval = artifacts.require('MaximumQuantityPerInterval');
 
-module.exports = async function (deployer) {
-
+module.exports = async function(deployer) {
 	// via https://ethereum.stackexchange.com/a/30579
 	// TODO make a nice loop here through all ProofTypes in /contracts/proof without having to list them specifically?
 
 	await deployer.deploy(Fin4Main);
-	var Fin4MainInstance = await Fin4Main.deployed();
 
-	await Promise.all([
-		deployer.deploy(ImmediateAutoApproval, Fin4MainInstance.address),
-		deployer.deploy(ApprovalBySpecificAddress, Fin4MainInstance.address),
-		deployer.deploy(ApprovalByTokenCreator, Fin4MainInstance.address),
-		deployer.deploy(MinimumClaimingInterval, Fin4MainInstance.address),
-		deployer.deploy(Password, Fin4MainInstance.address),
-		deployer.deploy(MaximumQuantityPerInterval, Fin4MainInstance.address)
-	]);
+	const Fin4MainInstance = await Fin4Main.deployed();
 
-	var proofTypeInstances = await Promise.all([
-		ImmediateAutoApproval.deployed(),
-		ApprovalBySpecificAddress.deployed(),
-		ApprovalByTokenCreator.deployed(),
-		MinimumClaimingInterval.deployed(),
-		Password.deployed(),
-		MaximumQuantityPerInterval.deployed()
-	]);
+	const contracts = [
+		ImmediateAutoApproval,
+		ApprovalBySpecificAddress,
+		ApprovalByTokenCreator,
+		MinimumClaimingInterval,
+		Password,
+		MaximumQuantityPerInterval
+	];
 
-	await Promise.all([
-		Fin4MainInstance.addProofType(proofTypeInstances[0].address),
-		Fin4MainInstance.addProofType(proofTypeInstances[1].address),
-		Fin4MainInstance.addProofType(proofTypeInstances[2].address),
-		Fin4MainInstance.addProofType(proofTypeInstances[3].address),
-		Fin4MainInstance.addProofType(proofTypeInstances[4].address),
-		Fin4MainInstance.addProofType(proofTypeInstances[5].address)
-	]);
+	await Promise.all(contracts.map(contract => deployer.deploy(contract, Fin4MainInstance.address)));
+
+	const proofTypeInstances = await Promise.all(contracts.map(contract => contract.deployed()));
+
+	await Promise.all(
+		proofTypeInstances.map(proofTypeInstance => Fin4MainInstance.addProofType(proofTypeInstance.address))
+	);
 };
