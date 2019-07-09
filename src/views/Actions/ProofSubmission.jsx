47c0b76d6cd3fd3f0fc5d6bc26892866a4608d7f
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-07-09 09:29:45 +0200

    Finish switching to new getContractData() from @simonzachau

--- src/views/Actions/ProofSubmission.jsx
+++ src/views/Actions/ProofSubmission.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Typography, Divider } from '@material-ui/core';
-import ContractData from '../../components/ContractData';
 import ContractForm from '../../components/ContractForm';
 import styled from 'styled-components';
 import colors from '../../config/colors-config';
@@ -13,94 +12,71 @@ class ProofSubmission extends Component {
 	constructor(props, context) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			proofData: null
+		};
 
-		/*getContractData(this.props.tokenAddress, 'Fin4Token.json', 'getClaim', [this.props.claimId], context.drizzle)
+		getContractData(this.props.tokenAddress, 'Fin4Token.json', 'getClaim', [this.props.claimId], context.drizzle)
 			.then(({ 7: requiredProofTypes, 8: proofTypeStatuses }) => {
-				this.proofTypeStatusesObj = {};
+				var proofTypeStatusesObj = {};
 				for (var i = 0; i < requiredProofTypes.length; i++) {
-					this.proofTypeStatusesObj[requiredProofTypes[i]] = {};
-					this.proofTypeStatusesObj[requiredProofTypes[i]].approved = proofTypeStatuses[i];
+					proofTypeStatusesObj[requiredProofTypes[i]] = {};
+					proofTypeStatusesObj[requiredProofTypes[i]].isApproved = proofTypeStatuses[i];
 				}
 				return requiredProofTypes.map((address, index) => {
 					return getContractData('Fin4Main', 'Fin4Main.json', 'getProofTypeInfo', [address], context.drizzle)
 						.then(({1: name, 2: description, 3: submitProofMethodArgsCount }) => {
-							const isApproved = this.proofTypeStatusesObj[address].approved;
-
-							return ({address, name, description});
-
+							return ({
+								address: address,
+								name: name,
+								description: description,
+								submitProofMethodArgsCount: submitProofMethodArgsCount,
+								isApproved: proofTypeStatusesObj[address].isApproved
+							});
 						});
 				})
 			})
 			.then(data => Promise.all(data))
-			.then(console.log);*/
+			.then(data => {
+				this.setState({proofData: data});
+			});
 	}
 
-	requiredProofTypeAddresses = ({ 7: requiredProofTypes, 8: proofTypeStatuses }) => {
-		this.proofTypeStatusesObj = {};
-		for (var i = 0; i < requiredProofTypes.length; i++) {
-			this.proofTypeStatusesObj[requiredProofTypes[i]] = {};
-			this.proofTypeStatusesObj[requiredProofTypes[i]].approved = proofTypeStatuses[i];
+	render() {
+		if (this.state.proofData === null) {
+			return "";
 		}
-
-		this.getProofTypeInfoAndShowForm = ({ 0: address, 1: name, 2: description, 3: submitProofMethodArgsCount }) => {
-			const isApproved = this.proofTypeStatusesObj[address].approved;
-
-			return (
-				<>
-					<Status isApproved={isApproved}>
-						{isApproved
-							? `The proof "${name}" was submitted successfully.`
-							: `Your claim requires you to fill out the following form: ${description}.`}
-					</Status>
-					{!isApproved && (
-						<ContractForm
-							contractAddress={address}
-							contractJson={name + '.json'}
-							method="submitProof"
-							// submitProof() occurs multiple times with different args in ProofTypes that inherit from each other.
-							// ContractForm would just take the first one in the abi that matches the method-name. Thats why another
-							// identifier is necessary to pick the right method. Probably by specific types would be even better.
-							methodArgsCount={submitProofMethodArgsCount}
-							fixArgs={{
-								tokenAdrToReceiveProof: this.props.tokenAddress,
-								claimId: this.props.claimId + ''
-							}}
-						/>
-					)}
-				</>
-			);
-		};
-
 		return (
 			<>
-				{requiredProofTypes.map((address, index) => {
+				{this.state.proofData.map((proofObj, index) => {
 					return (
 						<>
 							{index > 0 && <Divider variant="middle" style={{ margin: '50px 0' }} />}
-							<ContractData
-								key={index}
-								contractName="Fin4Main"
-								method="getProofTypeInfo"
-								methodArgs={[address]}
-								callback={this.getProofTypeInfoAndShowForm}
-							/>
+							<Status isApproved={proofObj.isApproved}>
+								{proofObj.isApproved
+									? `The proof "${proofObj.name}" was submitted successfully.`
+									: `Your claim requires you to fill out the following form: ${proofObj.description}.`}
+							</Status>
+							{!proofObj.isApproved && (
+								<ContractForm
+									contractAddress={proofObj.address}
+									contractJson={proofObj.name + '.json'}
+									method="submitProof"
+									// submitProof() occurs multiple times with different args in ProofTypes that inherit from each other.
+									// ContractForm would just take the first one in the abi that matches the method-name. Thats why another
+									// identifier is necessary to pick the right method. Probably by specific types would be even better.
+									methodArgsCount={proofObj.submitProofMethodArgsCount}
+									fixArgs={{
+										tokenAdrToReceiveProof: this.props.tokenAddress,
+										claimId: this.props.claimId + ''
+									}}
+								/>
+							)}
 						</>
 					);
 				})}
 			</>
-		);
-	};
-
-	render() {
-		return (
-			<ContractData
-				contractAddress={this.props.tokenAddress}
-				method="getClaim"
-				methodArgs={[this.props.claimId]}
-				callback={this.requiredProofTypeAddresses}
-			/>
-		);
+		)
 	}
 }
 
