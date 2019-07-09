bdf0aeb8dd0cf049d27f79efa21d653d0aa3dd6c
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-07-09 21:56:46 +0200

    Make Messages.jsx use getContractData()

--- src/views/Actions/Messages.jsx
+++ src/views/Actions/Messages.jsx
@@ -1,60 +1,89 @@
 import React, { Component } from 'react';
 import ContractData from '../../components/ContractData';
 import Box from '../../components/Box';
+import { drizzleConnect } from 'drizzle-react';
+import PropTypes from 'prop-types';
+import { getContractData } from '../../components/ContractData';
 
 class Messages extends Component {
-	showMessage = ({ 0: messageType, 1: sender, 2: message, 3: fulfillmentAddress, 4: proofTypeName }) => {
-		if (messageType === '1') {
+	constructor(props, context) {
+		super(props);
+
+		this.state = {
+			messages: []
+		};
+
+		getContractData('Fin4Main', 'Fin4Main.json', 'getMyMessagesCount', [], context.drizzle)
+			.then(data => {
+				var messageCount = Number(data);
+				var messageIndices = [];
+				for (var i = 0; i < messageCount; i++) {
+					messageIndices.push(i);
+				}
+				return messageIndices.map(index => {
+				 return getContractData('Fin4Main', 'Fin4Main.json', 'getMyMessage', [index], context.drizzle)
+					.then(({ 0: messageType, 1: sender, 2: message, 3: fulfillmentAddress, 4: proofTypeName }) => {
+						return {
+							messageType: messageType,
+							sender: sender,
+							message: message,
+							fulfillmentAddress: fulfillmentAddress,
+							proofTypeName: proofTypeName
+						};
+					})
+				});
+			})
+			.then(data => Promise.all(data))
+			.then(data => {
+				this.setState({ messages: data });
+			});
+	}
+
+	showMessage = msg => {
+		if (msg.messageType === '1') {
 			// INFO
-			return <p>{message}</p>;
+			return <p>{msg.message}</p>;
 		}
 		return (
 			// messageType === 0: APPROVE
 			<p>
-				{message}
-				<br></br>Requested by: <i>{sender}</i>
+				{msg.message}
+				<br></br>Requested by: <i>{msg.sender}</i>
 				<br></br>
 				<b>
-					<a href={`/approving?fulfillmentAddress=${fulfillmentAddress}&proofTypeName=${proofTypeName}`}>approve</a>
+					<a href={`/approving?fulfillmentAddress=${msg.fulfillmentAddress}&proofTypeName=${msg.proofTypeName}`}>approve</a>
 				</b>
 			</p>
 		);
 	};
 
-	showMyMessages = data => {
-		var messageCount = Number(data);
-		if (messageCount === 0) {
-			return <></>;
-		}
-		var messageIndices = [];
-		for (var i = 0; i < messageCount; i++) {
-			messageIndices.push(i);
+	render() {
+		if (this.state.messages.length < 1) {
+			return "";
 		}
 		return (
-			messageIndices.length > 0 && (
-				<Box title="Messages">
-					{messageIndices.map(index => {
-						return (
-							<div>
-								<ContractData
-									key={index}
-									contractName="Fin4Main"
-									method="getMyMessage"
-									methodArgs={[index]}
-									callback={this.showMessage}
-								/>
-								<hr></hr>
-							</div>
-						);
-					})}
-				</Box>
-			)
-		);
-	};
-
-	render() {
-		return <ContractData contractName="Fin4Main" method="getMyMessagesCount" callback={this.showMyMessages} />;
+			<Box title="Messages">
+				{this.state.messages.map(message => {
+					return (
+						<div>
+							{this.showMessage(message)}
+							<hr></hr>
+						</div>
+					);
+				})}
+			</Box>
+		)
 	}
 }
 
-export default Messages;
+Messages.contextTypes = {
+	drizzle: PropTypes.object
+};
+
+const mapStateToProps = state => {
+	return {
+		contracts: state.contracts
+	};
+};
+
+export default drizzleConnect(Messages, mapStateToProps);
