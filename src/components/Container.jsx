d2019a69317ae61d068cdabb698ad5ad010300ab
Simon <zachau@in.tum.de>
2019-07-10 16:17:30 +0200

    remove css

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,14 +1,6 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-	/*text-align: center;
-	> div {
-		display: inline-block;
-		vertical-align: text-top;
-		> * {
-			text-align: left;
-		}
-	}*/
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: center;
