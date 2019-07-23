77889b0f5a78ae4a65bd51a47dd66b9ad0bb6232
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-07-23 16:38:59 +0200

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-var HDWalletProvider = require("truffle-hdwallet-provider");
+var HDWalletProvider = require('truffle-hdwallet-provider');
 
 var keys;
 try {
 	keys = require('./src/config/ethereum-keys.json');
-} catch(err) {
-	console.log("./src/config/ethereum-keys.json not found");
+} catch (err) {
+	console.log('./src/config/ethereum-keys.json not found');
 }
 
 module.exports = {
@@ -18,27 +18,33 @@ module.exports = {
 		},
 		ropsten: {
 			provider: function() {
-			  return new HDWalletProvider(keys.MNEMONIC, 'https://ropsten.infura.io/v3/' + keys.INFURA_API_KEY)
+				return new HDWalletProvider(keys.MNEMONIC, 'https://ropsten.infura.io/v3/' + keys.INFURA_API_KEY);
 			},
 			network_id: 3,
 			gas: 4465030,
 			gasPrice: 10000000000
 		},
-		goerli_infura: {
+		rinkeby: {
 			provider: function() {
-				return new HDWalletProvider(keys.MNEMONIC, 'https://goerli.infura.io/v3/' + keys.INFURA_API_KEY)
-			  },
-			  network_id: 5,
-			  gas: 4465030,
-			  gasPrice: 10000000000
+				return new HDWalletProvider(keys.MNEMONIC, 'https://rinkeby.infura.io/v3/' + keys.INFURA_API_KEY);
+			},
+			network_id: 4,
+			gas: 6465030,
+			gasPrice: 10000000000
 		},
-		goerli_avado: {
+		goerli: {
 			provider: function() {
-				return new HDWalletProvider(keys.MNEMONIC, 'http://62.12.152.38:8548') //http://gatex.cysec.systems:8545  http://172.20.0.103:8545
-			  },
-			  network_id: 5,
-			  gas: 4465030,
-			  gasPrice: 10000000000
+				return new HDWalletProvider(keys.MNEMONIC, 'https://goerli.infura.io/v3/' + keys.INFURA_API_KEY);
+			},
+			network_id: 5,
+			gas: 7465030,
+			gasPrice: 10000000000
+		}
+	},
+	solc: {
+		optimizer: {
+			enabled: true,
+			runs: 200
 		}
 	}
 };
