1f77fcf41dbbcc0e78d72821d32e23cf83127234
Simon <zachau@in.tum.de>
2019-07-26 19:55:58 +0200

    remove outcommented code

--- src/components/Contractor.jsx
+++ src/components/Contractor.jsx
@@ -1,11 +1,5 @@
 import { Fin4MainAddress } from '../config/DeployedAddresses.js';
 
-/*
-var getContractByName = function(drizzle, contractName) { // those defined in drizzle-config.js
-	return getContractByAddress(drizzle, drizzle.contracts[contractName].address, contractName)
-};
-*/
-
 const getContract = (contractAddress, contractName) => {
 	const contract = require('truffle-contract');
 	const json = require('../build/contracts/' + contractName + '.json');
@@ -31,34 +25,6 @@ const getContractData = (contract, contractJson, method, methodArgs) => {
 			});
 	});
 };
-
-/*
-const getContractData = (contract, contractJson, method, methodArgs, drizzle) => {
-	// add contract if not yet added
-	if (!Object.keys(drizzle.contracts).includes(contract)) {
-		console.log(`adding contract ${contract}`);
-		const tokenJson = require('../build/contracts/' + contractJson);
-		const web3 = new Web3(window.web3.currentProvider);
-		drizzle.addContract({
-			contractName: contract,
-			web3Contract: new web3.eth.Contract(tokenJson.abi, contract)
-		});
-	}
-	return new Promise((resolve, reject) => {
-		// addContract() needs time and has no callback -> try every 1ms until successful
-		var tryAgain = setInterval(() => {
-			if (Object.keys(drizzle.contracts).includes(contract)) {
-				console.log(`found contract ${contract}`);
-				clearInterval(tryAgain);
-				// apply method
-				resolve(drizzle.contracts[contract].methods[method](...methodArgs).call());
-			} else {
-				console.log(`trying to find contract ${contract}`);
-			}
-		}, 1);
-	});
-};
-*/
 
 const getAllTokenTypes = () => {
 	return new Promise((resolve, reject) => {
