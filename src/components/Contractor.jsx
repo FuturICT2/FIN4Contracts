a56f6ee81d30da7a57298e33f77bc6044bb73a00
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-07-29 14:12:10 +0200

    Delete unused methods
    might recycle them later on though

--- src/components/Contractor.jsx
+++ src/components/Contractor.jsx
@@ -35,39 +35,4 @@ const getAllActionTypes = () => {
 		.then(data => Promise.all(data));
 };
 
-const getNetworkName = () => {
-	return new Promise((resolve, reject) => {
-		window.web3.version.getNetwork((err, netId) => {
-			switch (netId) {
-				case '1':
-					resolve('MainNet');
-					break;
-				case '3':
-					resolve('Ropsten');
-					break;
-				case '4':
-					resolve('Rinkeby');
-					break;
-				case '5':
-					resolve('Goerli');
-					break;
-				case '42':
-					resolve('Kovan');
-					break;
-				default:
-					resolve('Unknown');
-			}
-		});
-	});
-};
-
-const getNetworkBalance = () => {
-	return new Promise((resolve, reject) => {
-		let currentAccount = window.web3.currentProvider.selectedAddress;
-		window.web3.eth.getBalance(currentAccount, (error, result) => {
-			resolve(result);
-		});
-	});
-};
-
-export { getContractData, getContract, getAllActionTypes, getNetworkName, getNetworkBalance };
+export { getContractData, getContract, getAllActionTypes };
