a1e60647f3580d6c87119a277afaeee443c7a3cb
Sergiu <sergiu.soima@tum.de>
2019-08-07 13:43:31 +0200

    New AcceptedTokens view: build table for listings

--- src/views/AcceptedTokens/AcceptedTokens.jsx
+++ src/views/AcceptedTokens/AcceptedTokens.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
-import Container from '../../components/Container';
+import Box from '../../components/Box';
+import Table from '../../components/Table';
+import TableRow from '../../components/TableRow';
 
 class AcceptedTokens extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			listings: [
+				{
+					name: 'foo1',
+					info: 'info1'
+				},
+				{
+					name: 'foo2',
+					info: 'info2'
+				}
+			]
+		};
+
+		// TODO getContractData()
+	}
+
 	render() {
-		return <Container>TODO</Container>;
+		return (
+			<center>
+				<Box title="Listings">
+					<Table headers={['Name', 'Info']}>
+						{this.state.listings.map((entry, index) => {
+							return (
+								<TableRow
+									key={index}
+									data={{
+										name: entry.name,
+										info: entry.info
+									}}
+								/>
+							);
+						})}
+					</Table>
+				</Box>
+			</center>
+		);
 	}
 }
 
