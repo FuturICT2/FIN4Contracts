3a6558b7e243eb5a97a0378bc306e4ae2a9491d0
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-09-15 18:43:49 +0200

    Get ready for pilot: hide more/TCR, just have create and claim views

---
+++ src/views/Actions/Create.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Container from '../../components/Container';
+import TypeCreation from './TypeCreation';
+
+class Create extends Component {
+	render() {
+		return (
+			<Container>
+				<TypeCreation />
+			</Container>
+		);
+	}
+}
+
+export default Create;
