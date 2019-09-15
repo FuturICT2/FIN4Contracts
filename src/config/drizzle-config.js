6f37d6fdf895f95272bfa3655e27c02029fca612
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-09-15 19:39:01 +0200

    Add Fin4Main contract via address
    important to make deployment independent of having run truffle migrate beforehand on that deployment machine or not

--- src/config/drizzle-config.js
+++ src/config/drizzle-config.js
@@ -1,4 +1,8 @@
 import Fin4Main from '../build/contracts/Fin4Main.json';
+import { Fin4MainAddress } from './DeployedAddresses.js';
+
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'));
 
 const drizzleConfig = {
 	web3: {
@@ -8,7 +12,12 @@ const drizzleConfig = {
 			url: 'ws://127.0.0.1:7545'
 		}
 	},
-	contracts: [Fin4Main],
+	contracts: [
+		{
+			contractName: 'Fin4Main',
+			web3Contract: new web3.eth.Contract(Fin4Main.abi, Fin4MainAddress)
+		}
+	],
 	events: {
 		Fin4Main: ['Fin4TokenCreated', 'ClaimSubmitted', 'ClaimApproved']
 	},
