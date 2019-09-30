5ea89ed999d7521be48ada5618703a71de9e346a
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-09-30 10:04:20 +0200

    Convert TokenCreation from Class to Functional Component

--- src/views/Actions/TokenCreation.jsx
+++ src/views/Actions/TokenCreation.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ContractForm from '../../components/ContractForm';
 import Box from '../../components/Box';
 import Modal from '../../components/Modal';
@@ -9,73 +9,65 @@ import PropTypes from 'prop-types';
 import Currency from '../../components/Currency';
 import { Fin4MainAddress } from '../../config/DeployedAddresses.js';
 
-class TokenCreation extends Component {
-	constructor(props) {
-		super(props);
+function TokenCreation(props) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-		this.state = {
-			isModalOpen: false
-		};
-	}
-
-	toggleModal = () => {
-		this.setState({ isModalOpen: !this.state.isModalOpen });
+	const toggleModal = () => {
+		setIsModalOpen(!isModalOpen);
 	};
 
-	render() {
-		return (
-			<>
-				<Box title="Create a new token">
-					<ContractForm
-						contractAddress={Fin4MainAddress}
-						contractName="Fin4Main"
-						method="createNewToken"
-						multiSelectOptions={Object.keys(this.props.proofTypes).map(addr => this.props.proofTypes[addr])}
-						labels={['Name', 'Symbol', 'Description', 'Proof Types']}
-						hideArgs={{
-							paramValues: 'paramValues',
-							paramValuesIndices: 'paramValuesIndices'
-						}}
-						helperModalTriggers={[null, null, null, this.toggleModal]}
-					/>
-				</Box>
-				<Modal isOpen={this.state.isModalOpen} handleClose={this.toggleModal} title="Proof Types">
-					<Table headers={['Name', 'Description']}>
-						<>
-							{Object.keys(this.props.proofTypes).map((addr, index) => {
-								let proofType = this.props.proofTypes[addr];
-								return (
-									<TableRow
-										key={'proof_' + index}
-										data={{
-											name: proofType.label,
-											description: proofType.description
-										}}
-									/>
-								);
-							})}
-						</>
-					</Table>
-				</Modal>
-				<Box title="Tokens you created">
-					<Table headers={['Name']}>
-						{Object.keys(this.props.fin4Tokens).map((addr, index) => {
-							let token = this.props.fin4Tokens[addr];
+	return (
+		<>
+			<Box title="Create a new token">
+				<ContractForm
+					contractAddress={Fin4MainAddress}
+					contractName="Fin4Main"
+					method="createNewToken"
+					multiSelectOptions={Object.keys(props.proofTypes).map(addr => props.proofTypes[addr])}
+					labels={['Name', 'Symbol', 'Description', 'Proof Types']}
+					hideArgs={{
+						paramValues: 'paramValues',
+						paramValuesIndices: 'paramValuesIndices'
+					}}
+					helperModalTriggers={[null, null, null, toggleModal]}
+				/>
+			</Box>
+			<Modal isOpen={isModalOpen} handleClose={toggleModal} title="Proof Types">
+				<Table headers={['Name', 'Description']}>
+					<>
+						{Object.keys(props.proofTypes).map((addr, index) => {
+							let proofType = props.proofTypes[addr];
 							return (
 								<TableRow
-									key={'token_' + index}
+									key={'proof_' + index}
 									data={{
-										name: <Currency symbol={token.symbol} name={token.name} />
-										// edit: 'TODO'
+										name: proofType.label,
+										description: proofType.description
 									}}
 								/>
 							);
 						})}
-					</Table>
-				</Box>
-			</>
-		);
-	}
+					</>
+				</Table>
+			</Modal>
+			<Box title="Tokens you created">
+				<Table headers={['Name']}>
+					{Object.keys(props.fin4Tokens).map((addr, index) => {
+						let token = props.fin4Tokens[addr];
+						return (
+							<TableRow
+								key={'token_' + index}
+								data={{
+									name: <Currency symbol={token.symbol} name={token.name} />
+									// edit: 'TODO'
+								}}
+							/>
+						);
+					})}
+				</Table>
+			</Box>
+		</>
+	);
 }
 
 TokenCreation.contextTypes = {
