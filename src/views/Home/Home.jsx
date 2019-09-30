d4ac72ec48934c57ee3a6ba4e30912fafa5b84ea
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-09-30 07:48:09 +0200

    Convert Home class to functional component
    following this guide https://medium.com/@olinations/10-steps-to-convert-a-react-class-component-to-a-functional-component-with-hooks-ab198e0fa139

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -9,82 +9,80 @@ import styled from 'styled-components';
 import Currency from '../../components/Currency';
 const axios = require('axios');
 
-class Home extends Component {
-	render() {
-		return (
-			<Container>
-				<Messages />
-				<Box title="Your token balances">
-					{Object.keys(this.props.usersBalances).length === 0 ? (
-						<NoTokens>You haven't sucessfully claimed any tokens yet.</NoTokens>
-					) : (
-						<Table headers={['Name', 'Balance']}>
-							{Object.keys(this.props.usersBalances).map((tokenAddr, index) => {
-								let token = this.props.store.getState().fin4Store.fin4Tokens[tokenAddr];
-								return (
-									<TableRow
-										key={'balance_' + index}
-										data={{
-											name: <Currency symbol={token.symbol} name={token.name} />,
-											balance: this.props.usersBalances[tokenAddr]
-										}}
-									/>
-								);
-							})}
-						</Table>
-					)}
-				</Box>
-				<Box title="About you">
-					<p style={{ fontFamily: 'arial' }}>
-						Your public address:
-						<br />
-						<small>
-							{this.props.defaultAccount === null ? (
-								'Info not yet available'
-							) : (
-								// TODO make network-generic
-								<a href={'https://rinkeby.etherscan.io/address/' + this.props.defaultAccount} target="_blank">
-									{this.props.defaultAccount}
-								</a>
-							)}
-						</small>
-					</p>
-					<a
-						href="#"
-						onClick={() => {
-							let url = window.location.href;
-							// TODO make this URL slicing nicer!
-							if (url.endsWith('#')) {
-								url = url.substring(0, url.length - 1);
-							}
-							if (url.endsWith(':3000/')) {
-								url = url.substr(0, url.length - 6);
-							} else {
-								// = http://demo.finfour.net/
-								url = url.substring(0, url.length - 1);
-							}
-							let recipient = this.props.defaultAccount;
-							let networkID = window.ethereum.networkVersion;
-							let encodedURL = url + ':4000/faucet?recipient=' + recipient + '&networkID=' + networkID;
-							console.log('Calling faucet server: ' + encodedURL);
-							axios
-								.get(encodedURL)
-								.then(response => {
-									console.log('Successfully called faucet server. Response: ' + response.data);
-									alert(response.data);
-								})
-								.catch(error => {
-									console.log('Error calling faucet server', error);
-									alert('Failed to request Ether');
-								})
-								.finally(() => {});
-						}}>
-						<RequestEth>Request Ether</RequestEth>
-					</a>
-				</Box>
-			</Container>
-		);
-	}
+function Home(props) {
+	return (
+		<Container>
+			<Messages />
+			<Box title="Your token balances">
+				{Object.keys(props.usersBalances).length === 0 ? (
+					<NoTokens>You haven't sucessfully claimed any tokens yet.</NoTokens>
+				) : (
+					<Table headers={['Name', 'Balance']}>
+						{Object.keys(props.usersBalances).map((tokenAddr, index) => {
+							let token = props.store.getState().fin4Store.fin4Tokens[tokenAddr];
+							return (
+								<TableRow
+									key={'balance_' + index}
+									data={{
+										name: <Currency symbol={token.symbol} name={token.name} />,
+										balance: props.usersBalances[tokenAddr]
+									}}
+								/>
+							);
+						})}
+					</Table>
+				)}
+			</Box>
+			<Box title="About you">
+				<p style={{ fontFamily: 'arial' }}>
+					Your public address:
+					<br />
+					<small>
+						{props.defaultAccount === null ? (
+							'Info not yet available'
+						) : (
+							// TODO make network-generic
+							<a href={'https://rinkeby.etherscan.io/address/' + props.defaultAccount} target="_blank">
+								{props.defaultAccount}
+							</a>
+						)}
+					</small>
+				</p>
+				<a
+					href="#"
+					onClick={() => {
+						let url = window.location.href;
+						// TODO make this URL slicing nicer!
+						if (url.endsWith('#')) {
+							url = url.substring(0, url.length - 1);
+						}
+						if (url.endsWith(':3000/')) {
+							url = url.substr(0, url.length - 6);
+						} else {
+							// = http://demo.finfour.net/
+							url = url.substring(0, url.length - 1);
+						}
+						let recipient = props.defaultAccount;
+						let networkID = window.ethereum.networkVersion;
+						let encodedURL = url + ':4000/faucet?recipient=' + recipient + '&networkID=' + networkID;
+						console.log('Calling faucet server: ' + encodedURL);
+						axios
+							.get(encodedURL)
+							.then(response => {
+								console.log('Successfully called faucet server. Response: ' + response.data);
+								alert(response.data);
+							})
+							.catch(error => {
+								console.log('Error calling faucet server', error);
+								alert('Failed to request Ether');
+							})
+							.finally(() => {});
+					}}>
+					<RequestEth>Request Ether</RequestEth>
+				</a>
+			</Box>
+		</Container>
+	);
 }
 
 const RequestEth = styled.div`
