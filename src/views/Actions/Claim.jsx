22525503056df3d2a4ade1cc886b18b66271b5ed
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-10-01 09:22:48 +0200

    Remove standalone ClaimToken.jsx, integrate it one level up instead

--- src/views/Actions/Claim.jsx
+++ src/views/Actions/Claim.jsx
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from '../../components/Container';
+import ContractForm from '../../components/ContractForm';
+import Box from '../../components/Box';
 import PreviousClaims from './PreviousClaims';
-import ClaimToken from './ClaimToken';
+import { drizzleConnect } from 'drizzle-react';
+import { Fin4MainAddress } from '../../config/DeployedAddresses';
+import { useTranslation } from 'react-i18next';
 
-class Claim extends Component {
-	render() {
-		return (
-			<Container>
-				<div>
-					<ClaimToken />
-				</div>
-				<PreviousClaims />
-			</Container>
-		);
-	}
+function Claim(props) {
+	const { t, i18n } = useTranslation();
+	return (
+		<Container>
+			<div>
+				<Box title={t('claim-tokens')}>
+					<ContractForm
+						contractAddress={Fin4MainAddress}
+						contractName="Fin4Main"
+						method="submitClaim"
+						labels={[t('token-type'), t('quantity'), t('date'), t('comment')]}
+						singleSelectOptions={props.fin4Tokens}
+					/>
+				</Box>
+			</div>
+			<PreviousClaims />
+		</Container>
+	);
 }
 
-export default Claim;
+const mapStateToProps = state => {
+	return {
+		contracts: state.contracts,
+		fin4Tokens: state.fin4Store.fin4Tokens
+	};
+};
+
+export default drizzleConnect(Claim, mapStateToProps);
