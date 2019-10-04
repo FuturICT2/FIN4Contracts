33f2ab8048a78601e9137c27f091a80ae12e3e67
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-10-04 14:30:44 +0200

    Use Router-Link instead of Button

--- src/views/Actions/TokenOverview.jsx
+++ src/views/Actions/TokenOverview.jsx
@@ -5,8 +5,8 @@ import TableRow from '../../components/TableRow';
 import { drizzleConnect } from 'drizzle-react';
 import Currency from '../../components/Currency';
 import { useTranslation } from 'react-i18next';
-import Button from '../../components/Button';
 import history from '../../components/history';
+import { Link } from 'react-router-dom';
 
 function TokenOverview(props) {
 	const { t, i18n } = useTranslation();
@@ -15,7 +15,7 @@ function TokenOverview(props) {
 		<>
 			{Object.keys(props.fin4Tokens).length > 0 && (
 				<Box title={t('all-tokens')} width="500px">
-					<Table headers={[t('token-name'), 'Description', 'Unit', 'Manage']}>
+					<Table headers={[t('token-name'), 'Description', 'Unit', 'Actions']}>
 						{Object.keys(props.fin4Tokens).map((addr, index) => {
 							let token = props.fin4Tokens[addr];
 							let tokenSite = '/token/view/' + token.symbol;
@@ -30,15 +30,12 @@ function TokenOverview(props) {
 											</small>
 										),
 										unit: <small>{token.unit}</small>,
-										action: (
-											<Button
-												title={'go to ' + tokenSite}
-												onClick={() => {
-													history.push(tokenSite);
-													// window.location.href = tokenSite;
-												}}>
-												More
-											</Button>
+										actions: (
+											<small style={{ color: 'blue', textDecoration: 'underline' }}>
+												<Link to={'/token/view/' + token.symbol}>Details</Link>
+												<br />
+												<Link to={'/token/claim/' + token.symbol}>Claim</Link>
+											</small>
 										)
 									}}
 								/>
