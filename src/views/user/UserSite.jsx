7863d25715e805053d10fe78fce57a901e60c9b2
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-10-07 12:34:36 +0200

    Message sending form is ready, backend missing so far

--- src/views/user/UserSite.jsx
+++ src/views/user/UserSite.jsx
@@ -1,24 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Box from '../../components/Box';
 import { drizzleConnect } from 'drizzle-react';
 import { useTranslation } from 'react-i18next';
 import Container from '../../components/Container';
+import Button from '../../components/Button';
+import TextField from '@material-ui/core/TextField';
+import MessageIcon from '@material-ui/icons/Message';
 
 function UserSite(props) {
 	const { t, i18n } = useTranslation();
 
 	const [userAddressViaURL, setUserAddressViaURL] = useState(null);
+	const msgText = useRef('');
 
 	useEffect(() => {
 		let userAddress = props.match.params.userAddress;
-		if (!userAddressViaURL && userAddress) {
+		if (userAddress && !userAddressViaURL) {
 			setUserAddressViaURL(userAddress);
 		}
 	});
 
 	return (
 		<Container>
-			<Box title="User Actions">TODO</Box>
+			<Box title="Send message to user">
+				<center>
+					<div>To user {userAddressViaURL}</div>
+					<br />
+					<TextField
+						label="Message text"
+						multiline
+						rows="4"
+						fullWidth
+						variant="outlined"
+						onChange={e => (msgText.current = e.target.value)}
+					/>
+					<br />
+					<br />
+					<Button
+						icon={MessageIcon}
+						onClick={() => {
+							alert('TODO: Send ' + msgText.current);
+						}}>
+						Send
+					</Button>
+					<br />
+					<br />
+				</center>
+			</Box>
 		</Container>
 	);
 }
