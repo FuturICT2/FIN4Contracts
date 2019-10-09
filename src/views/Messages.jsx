eab19093eb6f7bd953a7581d7406bb276b973929
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-10-09 13:55:25 +0200

    Refactor Messages to functional component and use new getContractData()

--- src/views/Messages.jsx
+++ src/views/Messages.jsx
@@ -1,113 +1,113 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Box from '../components/Box';
 import ContractForm from '../components/ContractForm';
 import { drizzleConnect } from 'drizzle-react';
 import PropTypes from 'prop-types';
-import { getContractData_deprecated } from '../components/Contractor';
+import { getContractData } from '../components/Contractor';
 import Button from '../components/Button';
 import Photo from '@material-ui/icons/Photo';
 import { Typography, Divider, Paper } from '@material-ui/core';
 import styled from 'styled-components';
-import { Fin4MainAddress } from '../config/DeployedAddresses.js';
+import { useTranslation } from 'react-i18next';
 
-class Messages extends Component {
-	constructor(props) {
-		super(props);
+function Messages(props, context) {
+	const { t } = useTranslation();
 
-		this.state = {
-			messages: []
-		};
+	const [messages, setMessages] = useState([]);
+	const messagesFetched = useRef(false);
 
-		if (props.store.getState().fin4Store.defaultAccount === null) {
+	useEffect(() => {
+		if (props.defaultAccount === null || !props.contracts.Fin4Messages || !props.contracts.Fin4Messages.initialized) {
 			return;
 		}
+		if (!messagesFetched.current) {
+			fetchMessages();
+		}
+	});
 
-		getContractData_deprecated(props, Fin4MainAddress, 'Fin4Main', 'getFin4MessagesAddress').then(
-			Fin4MessagesAddress => {
-				getContractData_deprecated(props, Fin4MessagesAddress, 'Fin4Messages', 'getMyMessagesCount')
-					.then(data => {
-						var messageCount = Number(data);
-						var messageIndices = [];
-						for (var i = 0; i < messageCount; i++) {
-							messageIndices.push(i);
+	const fetchMessages = () => {
+		messagesFetched.current = true;
+		let Fin4MessagesContract = context.drizzle.contracts.Fin4Messages;
+
+		getContractData(Fin4MessagesContract, props.defaultAccount, 'getMyMessagesCount')
+			.then(data => {
+				var messageCount = Number(data);
+				var messageIndices = [];
+				for (var i = 0; i < messageCount; i++) {
+					messageIndices.push(i);
+				}
+				return messageIndices.map(index => {
+					return getContractData(Fin4MessagesContract, props.defaultAccount, 'getMyMessage', index).then(
+						({
+							0: messageType,
+							1: sender,
+							2: message,
+							3: fulfillmentAddress,
+							4: proofTypeName,
+							5: hasBeenActedUpon,
+							6: attachment
+						}) => {
+							return {
+								messageType: messageType.toString(),
+								sender: sender,
+								message: message,
+								fulfillmentAddress: fulfillmentAddress,
+								proofTypeName: proofTypeName,
+								hasBeenActedUpon: hasBeenActedUpon,
+								attachment: attachment
+							};
 						}
-						return messageIndices.map(index => {
-							return getContractData_deprecated(props, Fin4MessagesAddress, 'Fin4Messages', 'getMyMessage', index).then(
-								({
-									0: messageType,
-									1: sender,
-									2: message,
-									3: fulfillmentAddress,
-									4: proofTypeName,
-									5: hasBeenActedUpon,
-									6: attachment
-								}) => {
-									return {
-										messageType: messageType.toString(),
-										sender: sender,
-										message: message,
-										fulfillmentAddress: fulfillmentAddress,
-										proofTypeName: proofTypeName,
-										hasBeenActedUpon: hasBeenActedUpon,
-										attachment: attachment
-									};
-								}
-							);
-						});
-					})
-					.then(data => Promise.all(data))
-					.then(data => {
-						this.setState({ messages: data });
-					});
-			}
-		);
-	}
+					);
+				});
+			})
+			.then(data => Promise.all(data))
+			.then(data => {
+				setMessages(data);
+			});
+	};
 
-	render() {
-		let hasNewMsgs =
-			this.state.messages.length > 0 && this.state.messages.filter(msg => !msg.hasBeenActedUpon).length > 0;
-		return (
-			hasNewMsgs && (
-				<Box title="Messages">
-					{this.state.messages
-						.filter(msg => !msg.hasBeenActedUpon)
-						.map((msg, index) => {
-							return (
-								<Message key={`${msg.proofTypeName}_${msg.fulfillmentAddress}_${index}`}>
-									<Typography color="textSecondary" variant="body2">
-										{msg.message}.
-									</Typography>
-									{msg.messageType === '0' && (
-										<>
-											<Divider style={{ margin: '10px 0' }} variant="middle" />
-											<Typography color="textSecondary" variant="body2">
-												Requested by {msg.sender}
-											</Typography>
-											<Divider style={{ margin: '10px 0' }} variant="middle" />
-											{msg.attachment && msg.attachment.length > 0 && (
-												<Button
-													center="true"
-													icon={Photo}
-													onClick={() => window.open('https://gateway.ipfs.io/ipfs/' + msg.attachment, '_blank')}>
-													Click to see the image
-												</Button>
-											)}
-											<ContractForm
-												buttonLabel="approve"
-												contractAddress={msg.fulfillmentAddress}
-												// instead of passing the proofTypeName, make an extra getName() call for that?
-												contractName={msg.proofTypeName}
-												method="receiveApprovalFromSpecificAddress"
-											/>
-										</>
-									)}
-								</Message>
-							);
-						})}
-				</Box>
-			)
-		);
-	}
+	return (
+		messages.length > 0 &&
+		messages.filter(msg => !msg.hasBeenActedUpon).length > 0 && (
+			<Box title="Messages">
+				{messages
+					.filter(msg => !msg.hasBeenActedUpon)
+					.map((msg, index) => {
+						return (
+							<Message key={`${msg.proofTypeName}_${msg.fulfillmentAddress}_${index}`}>
+								<Typography color="textSecondary" variant="body2">
+									{msg.message}.
+								</Typography>
+								{msg.messageType === '0' && (
+									<>
+										<Divider style={{ margin: '10px 0' }} variant="middle" />
+										<Typography color="textSecondary" variant="body2">
+											Requested by {msg.sender}
+										</Typography>
+										<Divider style={{ margin: '10px 0' }} variant="middle" />
+										{msg.attachment && msg.attachment.length > 0 && (
+											<Button
+												center="true"
+												icon={Photo}
+												onClick={() => window.open('https://gateway.ipfs.io/ipfs/' + msg.attachment, '_blank')}>
+												Click to see the image
+											</Button>
+										)}
+										<ContractForm
+											buttonLabel="approve"
+											contractAddress={msg.fulfillmentAddress}
+											// instead of passing the proofTypeName, make an extra getName() call for that?
+											contractName={msg.proofTypeName}
+											method="receiveApprovalFromSpecificAddress"
+										/>
+									</>
+								)}
+							</Message>
+						);
+					})}
+			</Box>
+		)
+	);
 }
 
 const Message = styled(Paper)`
@@ -125,7 +125,8 @@ Messages.contextTypes = {
 
 const mapStateToProps = state => {
 	return {
-		contracts: state.contracts
+		contracts: state.contracts,
+		defaultAccount: state.fin4Store.defaultAccount
 	};
 };
 
