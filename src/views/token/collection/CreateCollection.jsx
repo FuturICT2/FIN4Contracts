6409ab4cebb2890703dabfd7ee85299dc42c374e
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-10-13 21:54:21 +0200

    Use ContractForm to create new collection

--- src/views/token/collection/CreateCollection.jsx
+++ src/views/token/collection/CreateCollection.jsx
@@ -3,11 +3,16 @@ import Box from '../../../components/Box';
 import { drizzleConnect } from 'drizzle-react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
+import ContractForm from '../../../components/ContractForm';
 
 function CreateCollection(props, context) {
 	const { t } = useTranslation();
 
-	return <Box title="Create a new collection"></Box>;
+	return (
+		<Box title="Create a new collection">
+			<ContractForm contractName="Fin4Collections" method="createCollection" />
+		</Box>
+	);
 }
 
 CreateCollection.contextTypes = {
