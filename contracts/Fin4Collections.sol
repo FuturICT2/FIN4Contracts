971addc149ea7b1b4393a9b99965e8747bc6df09
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-10-13 21:12:52 +0200

    Add methods to Fin4Collections contract

--- contracts/Fin4Collections.sol
+++ contracts/Fin4Collections.sol
@@ -11,8 +11,8 @@ contract Fin4Collections {
     struct Collection {
         uint collectionId;
         address creator;
-        address[] tokens;
         address[] admins;
+        address[] tokens;
         string name;
         string identifier;
         string description;
@@ -20,6 +20,77 @@ contract Fin4Collections {
         string logoURL;
     }
 
+    uint public nextCollectionId = 0;
+    uint private INVALID_INDEX = 9999;
+
     mapping (uint => Collection) public collections;
+
+    function createCollection(string memory name, string memory identifier, string memory description) public returns(uint) {
+        Collection storage col = collections[nextCollectionId];
+        col.creator = msg.sender;
+        col.admins.push(msg.sender);
+        col.name = name;
+        col.identifier = identifier;
+        col.description = description;
+        nextCollectionId ++;
+        return nextCollectionId;
+    }
+
+    function getCollection(uint collectionId) public view returns(address, address[] memory, address[] memory,
+        string memory, string memory, string memory, string memory, string memory) {
+        Collection memory col = collections[collectionId];
+        return(col.creator, col.admins, col.tokens, col.name, col.identifier, col.description, col.color, col.logoURL);
+    }
+
+    function addAdmin(uint collectionId, address newAdmin) public {
+        require(collections[collectionId].creator == msg.sender, "Only the collection creator can add admins");
+        collections[collectionId].admins.push(newAdmin);
+    }
+
+    function removeAdmin(uint collectionId, address adminToRemove) public {
+        require(collections[collectionId].creator == msg.sender, "Only the collection creator can remove admins");
+        uint index = getIndexOfAdmin(collectionId, adminToRemove);
+        require(index != INVALID_INDEX, "Given address is not registered as admin on this collection");
+        delete collections[collectionId].admins[index];
+    }
+
+    function addTokens(uint collectionId, address[] memory newTokens) public {
+        Collection storage col = collections[collectionId];
+        uint adminIndex = getIndexOfAdmin(collectionId, msg.sender);
+        require(adminIndex != INVALID_INDEX, "Only collection-admins can add tokens");
+        for (uint i = 0; i < newTokens.length; i ++) {
+            col.tokens.push(newTokens[i]);
+        }
+    }
+
+    function removeToken(uint collectionId, address token) public {
+        uint adminIndex = getIndexOfAdmin(collectionId, msg.sender);
+        require(adminIndex != INVALID_INDEX, "Only collection-admins can remove tokens");
+        uint tokenIndex = getIndexOfToken(collectionId, token);
+        require(tokenIndex != INVALID_INDEX, "Token not contained in this collection, can't remove it");
+        delete collections[collectionId].tokens[tokenIndex];
+    }
+
+    // HELPER METHODS
+
+    function getIndexOfAdmin(uint collectionId, address admin) private view returns(uint) {
+        Collection memory col = collections[collectionId];
+        for (uint i = 0; i < col.admins.length; i ++) {
+            if (col.admins[i] == admin) {
+                return i;
+            }
+        }
+        return INVALID_INDEX;
+    }
+
+    function getIndexOfToken(uint collectionId, address token) private view returns(uint) {
+        Collection memory col = collections[collectionId];
+        for (uint i = 0; i < col.tokens.length; i ++) {
+            if (col.tokens[i] == token) {
+                return i;
+            }
+        }
+        return INVALID_INDEX;
+    }
 
 }
