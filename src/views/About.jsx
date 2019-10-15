8eeb8a2db012dccfd4f18611a562691a2988518c
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-10-15 12:56:57 +0200

    Add content to About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -13,12 +13,60 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function About(props, context) {
-	const { t } = useTranslation();
+	const { t, i18n } = useTranslation();
 
 	const classes = useStyles();
 	return (
 		<Container>
-			<Box title="About"></Box>
+			<Box title="About FIN4Xplorer">
+				<div className={classes.font}>
+					The Finance4.0 platform (Fin4) aspires to be a socio-technical artifact spanning a design space for a
+					socio-ecological financial system and for post-monetary concepts and experiments.
+					<br />
+					<br />
+					The research project is supported by{' '}
+					<a href="http://www.snf.ch/" target="_blank">
+						SNF
+					</a>
+					,{' '}
+					<a href="http://www.climate-kic.org/" target="_blank">
+						Climate-KIC
+					</a>{' '}
+					and powered by{' '}
+					<a href="https://coss.ethz.ch/" target="_blank">
+						COSS
+					</a>
+					.
+					<br />
+					<br />
+					The project is open source and welcomes contributors:{' '}
+					<a href="https://github.com/FuturICT2/FIN4Xplorer" target="_blank">
+						FIN4Xplorer
+					</a>
+					<br />
+					<br />
+					The documentation can be found{' '}
+					<a
+						href={
+							i18n.language === 'en'
+								? 'https://fin4xplorer.readthedocs.io/en/latest/'
+								: 'https://fin4xplorer.readthedocs.io/de/latest/'
+						}
+						target="_blank">
+						here
+					</a>
+					.
+					<br />
+					<br />
+					More info on{' '}
+					<a href="http://finfour.net/" target="_blank">
+						finfour.net
+					</a>
+					<br />
+					<br />
+					Contact: finfour@gmx.net
+				</div>
+			</Box>
 		</Container>
 	);
 }
