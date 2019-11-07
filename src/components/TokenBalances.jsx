07c4cf34ee0801878a25b11b99149c51db2a70ae
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-07 20:36:59 +0100

    Show REP/GOV in TokenBalances component if > 0

--- src/components/TokenBalances.jsx
+++ src/components/TokenBalances.jsx
@@ -6,16 +6,59 @@ import Currency from './Currency';
 import Box from './Box';
 import Table from './Table';
 import TableRow from './TableRow';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-function TokenBalances(props) {
+function TokenBalances(props, context) {
 	const { t } = useTranslation();
+
+	const noBalanceYet = balances => {
+		for (var addr in balances) {
+			if (balances.hasOwnProperty(addr)) {
+				if (balances[addr] > 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	};
+
+	const buildGovernanceTokenBalance = (contract, name, symbol) => {
+		let balance = props.usersFin4GovernanceTokenBalances[contract.address];
+		if (balance === 0) {
+			return;
+		}
+		return (
+			<TableRow
+				key={'balance_' + name}
+				data={{
+					name: (
+						<>
+							<span>
+								<Currency symbol={symbol} name={name} />
+							</span>
+							<FontAwesomeIcon icon={faInfoCircle} style={styles.infoIcon} />
+						</>
+					),
+					balance: balance
+				}}
+			/>
+		);
+	};
 
 	return (
 		<Box title={t('your-token-balances')}>
-			{Object.keys(props.usersFin4TokenBalances).length === 0 ? (
+			{noBalanceYet(props.usersFin4TokenBalances) && noBalanceYet(props.usersFin4GovernanceTokenBalances) ? (
 				<NoTokens>{t('no-tokens-yet')}</NoTokens>
 			) : (
 				<Table headers={[t('token-name'), t('token-balance')]} colWidths={[85, 15]}>
+					{props.contracts.GOV &&
+						props.contracts.GOV.initialized &&
+						buildGovernanceTokenBalance(context.drizzle.contracts.GOV, 'Fin4 Governance Token', 'GOV')}
+					{props.contracts.Fin4Reputation &&
+						props.contracts.Fin4Reputation.initialized &&
+						buildGovernanceTokenBalance(context.drizzle.contracts.Fin4Reputation, 'Fin4 Reputation Token', 'REP')}
 					{Object.keys(props.usersFin4TokenBalances).map((tokenAddr, index) => {
 						let token = props.fin4Tokens[tokenAddr];
 						return (
@@ -38,16 +81,31 @@ function TokenBalances(props) {
 	);
 }
 
+const styles = {
+	infoIcon: {
+		color: 'silver',
+		width: '16px',
+		height: '16px',
+		paddingLeft: '5px'
+	}
+};
+
 const NoTokens = styled.div`
 	font-family: arial;
 	text-align: center;
 	color: silver;
 `;
 
+TokenBalances.contextTypes = {
+	drizzle: PropTypes.object
+};
+
 const mapStateToProps = state => {
 	return {
 		usersFin4TokenBalances: state.fin4Store.usersFin4TokenBalances,
-		fin4Tokens: state.fin4Store.fin4Tokens
+		usersFin4GovernanceTokenBalances: state.fin4Store.usersFin4GovernanceTokenBalances,
+		fin4Tokens: state.fin4Store.fin4Tokens,
+		contracts: state.contracts
 	};
 };
 
