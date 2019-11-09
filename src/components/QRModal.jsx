4a01f83831636b9c6a8a3c6b015b0872f8687461
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-09 15:56:25 +0100

    Use material-ui immediate tooltip on public address
    Modelled after how MetaMask does this, only the arrow is missing

--- src/components/QRModal.jsx
+++ src/components/QRModal.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Modal from './Modal';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Tooltip from '@material-ui/core/Tooltip';
 var QRCode = require('qrcode.react');
 
 function QRModal(props) {
 	const { t } = useTranslation();
+
+	const [tooltipText, setTooltipText] = useState('Click to copy to clipboard');
 
 	return (
 		<Modal isOpen={props.isOpen} handleClose={props.handleClose} title="Your QR code" width="300px">
@@ -15,10 +18,12 @@ function QRModal(props) {
 				<br />
 				Your public address:
 				<br />
-				<CopyToClipboard text={props.publicAddress} onCopy={() => {}}>
-					<span title="Click to copy to clipboard" style={{ fontSize: 'x-small' }}>
-						{props.publicAddress}
-					</span>
+				<CopyToClipboard text={props.publicAddress} onCopy={() => setTooltipText('Copied!')}>
+					<Tooltip title={tooltipText}>
+						<span style={{ fontSize: 'x-small' }}>
+							<b>{props.publicAddress}</b>
+						</span>
+					</Tooltip>
 				</CopyToClipboard>
 			</center>
 		</Modal>
