83983a69fa2df0be33c922d271fb58f3cf2704aa
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-10 14:16:41 +0100

    Prepare import of token creation drafts

--- src/views/tokens/Token.jsx
+++ src/views/tokens/Token.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { drizzleConnect } from 'drizzle-react';
 import { useTranslation } from 'react-i18next';
 import Container from '../../components/Container';
 import TokenOverview from './TokenOverview';
 import Box from '../../components/Box';
-import { buildIconLabelLink } from '../../components/utils';
+import { buildIconLabelLink, buildIconLabelCallback } from '../../components/utils';
 import AddIcon from '@material-ui/icons/AddBox';
+import ImportIcon from '@material-ui/icons/ImportExport';
 
 function Token(props) {
 	const { t } = useTranslation();
+
+	const [tokenDrafts, setTokenDrafts] = useState([]);
+
+	const [uploadFileVisible, setUploadFileVisible] = useState(false);
+	const toggleUploadFileVisible = () => {
+		setUploadFileVisible(!uploadFileVisible);
+	};
+
+	const onSelectFile = file => {
+		toggleUploadFileVisible();
+		// TODO
+	};
+
 	return (
 		<Container>
 			<Box title={t('create-new-token')}>
 				{buildIconLabelLink('/token/create', <AddIcon />, 'Start a new token creation')}
+				{buildIconLabelCallback(toggleUploadFileVisible, <ImportIcon />, 'Import token creation draft')}
+				{uploadFileVisible && (
+					<>
+						<input
+							style={{ paddingLeft: '45px' }}
+							type="file"
+							onChange={e => onSelectFile(e.target.files[0])}
+							accept="application/json"
+						/>
+						<br />
+					</>
+				)}
 			</Box>
 			<TokenOverview />
 		</Container>
