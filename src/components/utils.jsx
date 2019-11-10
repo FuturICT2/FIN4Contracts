5030df00af7f201d729878f0e2ef783250568ca7
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-10 19:11:30 +0100

    Use Link for buildIconLabelCallback() too, to have consistent look and feel

--- src/components/utils.jsx
+++ src/components/utils.jsx
@@ -18,13 +18,13 @@ const buildIconLabelLink = (link, icon, label) => {
 const buildIconLabelCallback = (callback, icon, label) => {
 	return (
 		<>
-			<div
-				onClick={callback}
-				style={{ display: 'flex', alignItems: 'center', paddingLeft: '15px', fontFamily: 'arial', color: 'blue' }}>
-				{icon}
-				&nbsp;&nbsp;{label}
-			</div>
-			<br />
+			<Link to="#" onClick={callback} style={{ textDecoration: 'none' }}>
+				<div style={{ display: 'flex', alignItems: 'center', paddingLeft: '15px', fontFamily: 'arial' }}>
+					{icon}
+					&nbsp;&nbsp;{label}
+				</div>
+				<br />
+			</Link>
 		</>
 	);
 };
