61f3d3fbc5edaa3a32d6b8ad1c41eebdaf18a728
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-11 10:07:34 +0100

    Use material-ui stepper for the token creation process

--- src/views/tokens/TokenCreationProcess.jsx
+++ src/views/tokens/TokenCreationProcess.jsx
@@ -3,10 +3,30 @@ import { drizzleConnect } from 'drizzle-react';
 import { useTranslation } from 'react-i18next';
 import Container from '../../components/Container';
 import Box from '../../components/Box';
-import StepBasics from './creationProcess/StepBasics';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+	// from https://material-ui.com/components/steppers/
+	root: {
+		width: '100%'
+	},
+	backButton: {
+		marginRight: theme.spacing(1)
+	},
+	instructions: {
+		marginTop: theme.spacing(1),
+		marginBottom: theme.spacing(1)
+	}
+}));
 
 function TokenCreation(props, context) {
 	const { t } = useTranslation();
+	const classes = useStyles();
 
 	const [draft, setDraft] = useState(null);
 
@@ -18,12 +38,78 @@ function TokenCreation(props, context) {
 		setDraft(props.tokenCreationDrafts[draftId]);
 	});
 
+	const steps = ['Basics', 'Traits', 'Actions', 'Value', 'Proofs']; // Disbursement/Valuation instead of Value?
+
+	const getStepContent = stepIndex => {
+		switch (stepIndex) {
+			case 0:
+				return 'Basic infos';
+			case 1:
+				return 'Fundamental properties of the token';
+			case 2:
+				return 'For what action(s) can people claim this token?';
+			case 3:
+				return 'What quantity can be obtained per claim?';
+			case 4:
+				return 'Add proof types that users will have to provide';
+			default:
+				return '';
+		}
+	};
+
+	const [activeStep, setActiveStep] = useState(0);
+
+	const handleNext = () => {
+		setActiveStep(prevActiveStep => prevActiveStep + 1);
+	};
+
+	const handleBack = () => {
+		setActiveStep(prevActiveStep => prevActiveStep - 1);
+	};
+
+	const handleReset = () => {
+		setActiveStep(0);
+	};
+
 	return (
 		<>
 			{draft ? (
 				<Container>
 					<Box title="Token creation">
-						<StepBasics draft={draft} onSubmit={() => {}} />
+						<div className={classes.root}>
+							<Stepper activeStep={activeStep} alternativeLabel>
+								{steps.map(label => (
+									<Step key={label}>
+										<StepLabel>{label}</StepLabel>
+									</Step>
+								))}
+							</Stepper>
+							<center>
+								<Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+							</center>
+						</div>
+
+						<center style={{ padding: '10px 20px 30px 20px' }}>Components go here...</center>
+
+						<div>
+							<center style={{ paddingBottom: '20px' }}>
+								{activeStep === steps.length ? (
+									<span>
+										<Typography className={classes.instructions}>All steps completed</Typography>
+										<Button onClick={handleReset}>Reset</Button>
+									</span>
+								) : (
+									<div>
+										<Button disabled={activeStep === 0} onClick={handleBack} className={classes.backButton}>
+											Back
+										</Button>
+										<Button variant="contained" color="primary" onClick={handleNext}>
+											{activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+										</Button>
+									</div>
+								)}
+							</center>
+						</div>
 					</Box>
 				</Container>
 			) : (
