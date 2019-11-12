58420c596f963e859ba6c96cc7920f1a88b4d209
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-12 12:41:42 +0100

    Fill the info-popup about fundamental token properties

--- src/views/tokens/creationProcess/Step2Traits.jsx
+++ src/views/tokens/creationProcess/Step2Traits.jsx
@@ -107,7 +107,54 @@ function StepTraits(props) {
 			{draftId && (
 				<>
 					<Modal isOpen={isModalOpen} handleClose={toggleModal} title="Info" width="350px">
-						<div style={{ fontFamily: 'arial' }}>TODO</div>
+						<div style={{ fontFamily: 'arial' }}>
+							<b>is transferable</b>
+							<br />
+							Users who have a balance on this token, can transfer some or all of it to other users.
+							<br />
+							<br />
+							<b>is mintable</b>
+							<br />
+							Eligible users or smart contracts can "mint" any amount to a public address of their choosing. The total
+							supply of this token gets increased by that amount.
+							<br />
+							<br />
+							<b>you are minter</b>
+							<br />
+							You are allowed to mint this token. Note that this gives you the ability to circumvent the{' '}
+							claiming/proofing process that you demand from your users - you can directly mint yourself any amount.
+							Since everyone can see who are minters of a token, this might substantially reduce trust in your token.
+							<br />
+							<br />
+							<b>is burnable</b>
+							<br />
+							Users can burn some or all of their balance on this token. The burned amount gets deducted from their
+							balance and the total supply of the token shrinks by that amount.
+							<br />
+							<br />
+							<b>is capped</b>
+							<br />
+							Once the cap is reached, no more minting is possible on this token.
+							<br />
+							<br />
+							<b>Cap</b>
+							<br />
+							If the above is activated, this is the value of the cap.
+							<br />
+							<br />
+							<b>Decimals</b>
+							<br />
+							The digits by which your token is divisible. Zero means that users can only have natural numbers as
+							balance on your token and only amounts in natural numbers can be transferred.
+							<br />
+							<br />
+							<b>Initial supply</b>
+							<br />
+							Sets your initial balance on this token. It calls the mint function, so the total supply will also rise to
+							this amount.
+							<br />
+							<br />
+						</div>
 					</Modal>
 					<center>
 						<FontAwesomeIcon
