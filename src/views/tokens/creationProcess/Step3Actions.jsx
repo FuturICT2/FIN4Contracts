9328ced9744fb1f5ce30367fa6ca172a84d6a585
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-12 13:18:02 +0100

    Prepare Step3 Actions elements

--- src/views/tokens/creationProcess/Step3Actions.jsx
+++ src/views/tokens/creationProcess/Step3Actions.jsx
@@ -3,6 +3,10 @@ import { drizzleConnect } from 'drizzle-react';
 import { useTranslation } from 'react-i18next';
 import moment from 'moment';
 import StepsBottomNav from './StepsBottomNav';
+import { TextField } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import Modal from '../../../components/Modal';
 
 function StepActions(props) {
 	const { t } = useTranslation();
@@ -26,6 +30,11 @@ function StepActions(props) {
 		setDraftId(draft.id);
 	});
 
+	const [isModalOpen, setModalOpen] = useState(false);
+	const toggleModal = () => {
+		setModalOpen(!isModalOpen);
+	};
+
 	const submit = () => {
 		fields.current.lastModified = moment().valueOf();
 		// TODO
@@ -36,12 +45,34 @@ function StepActions(props) {
 		<>
 			{draftId && (
 				<>
+					<Modal isOpen={isModalOpen} handleClose={toggleModal} title="Info" width="350px">
+						TODO
+					</Modal>
+					<center>
+						<FontAwesomeIcon
+							icon={faInfoCircle}
+							style={styles.infoIcon}
+							onClick={() => {
+								toggleModal();
+							}}
+						/>
+					</center>
+					<br />
+					<TextField label="" multiline rows="4" fullWidth variant="outlined" onChange={e => {}} />
 					<StepsBottomNav nav={props.nav} handleNext={submit} />
 				</>
 			)}
 		</>
 	);
 }
+
+const styles = {
+	infoIcon: {
+		color: 'silver',
+		width: '20px',
+		height: '20px'
+	}
+};
 
 const mapStateToProps = state => {
 	return {};
