fbdcf1f6e422c2f8ce3df503db6934cd16d7ff71
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-14 17:24:34 +0100

    Wip: must distinguish btwn Fin4Token and Fin4TokenCapped

--- contracts/Fin4Token.sol
+++ contracts/Fin4Token.sol
@@ -5,9 +5,32 @@ import 'contracts/Fin4TokenBase.sol';
 
 contract Fin4Token is Fin4TokenBase, ERC20Plus {
 
-  constructor(string memory name, string memory symbol, string memory description, string memory unit, address tokenCreator)
-    ERC20Plus(name, symbol, 0, tokenCreator, false, true, true, 0)
-    Fin4TokenBase(description, unit, tokenCreator)
+  constructor(string memory name, string memory symbol, address tokenCreator, bool[] memory properties, uint[] memory values)
+    ERC20Plus(name, symbol, uint8(values[1]), tokenCreator, properties[2], properties[0], properties[1], values[2])
+    Fin4TokenBase()
+    public {}
+
+  // TODO is it possible to move these two functions to Fin4TokenBase?
+
+  function getTokenInfo(address user) public view returns(bool, bool, string memory, string memory,
+    string memory, string memory, uint256, uint) {
+    bool userIsCreator = user == tokenCreator;
+    bool userIsAdmin = userIsCreator; // TODO
+    return (userIsCreator, userIsAdmin, name(), symbol(), description, unit, totalSupply(), tokenCreationTime);
+  }
+
+  function getDetailedTokenInfo(address user) public view returns(address[] memory, uint, uint256, uint256, uint) {
+    return (requiredProofTypes, nextClaimId, balanceOf(user), totalSupply(), tokenCreationTime);
+  }
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+contract Fin4TokenCapped is Fin4TokenBase, ERC20PlusCapped {
+
+  constructor(string memory name, string memory symbol, address tokenCreator, bool[] memory properties, uint[] memory values)
+    ERC20PlusCapped(name, symbol, uint8(values[1]), tokenCreator, properties[2], values[0], properties[0], properties[1], values[2])
+    Fin4TokenBase()
     public {}
 
   function getTokenInfo(address user) public view returns(bool, bool, string memory, string memory,
@@ -20,5 +43,4 @@ contract Fin4Token is Fin4TokenBase, ERC20Plus {
   function getDetailedTokenInfo(address user) public view returns(address[] memory, uint, uint256, uint256, uint) {
     return (requiredProofTypes, nextClaimId, balanceOf(user), totalSupply(), tokenCreationTime);
   }
-
 }
