05e91a86a1c4e321abc43fea15e5d9d2274c51ee
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-14 17:27:24 +0100

    New default: token creator is not minter
    Otherwise they could mint themselves rich via command line
    
    Co-Authored-By: Marcus M Dapp <lists@digisus.org>

--- contracts/Fin4Token.sol
+++ contracts/Fin4Token.sol
@@ -8,7 +8,10 @@ contract Fin4Token is Fin4TokenBase, ERC20Plus {
   constructor(string memory name, string memory symbol, address tokenCreator, bool[] memory properties, uint[] memory values)
     ERC20Plus(name, symbol, uint8(values[1]), tokenCreator, properties[2], properties[0], properties[1], values[2])
     Fin4TokenBase()
-    public {}
+    public {
+      // Otherwise token creators can mint themselves rich via command line #ConceptualDecision
+      _removeMinter(tokenCreator);
+    }
 
   // TODO is it possible to move these two functions to Fin4TokenBase?
 
@@ -31,7 +34,9 @@ contract Fin4TokenCapped is Fin4TokenBase, ERC20PlusCapped {
   constructor(string memory name, string memory symbol, address tokenCreator, bool[] memory properties, uint[] memory values)
     ERC20PlusCapped(name, symbol, uint8(values[1]), tokenCreator, properties[2], values[0], properties[0], properties[1], values[2])
     Fin4TokenBase()
-    public {}
+    public {
+      _removeMinter(tokenCreator);
+    }
 
   function getTokenInfo(address user) public view returns(bool, bool, string memory, string memory,
     string memory, string memory, uint256, uint) {
