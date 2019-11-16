ef62385ef7ed0d948bf9dbc1d40d7a979205e319
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-17 00:41:21 +0100

    Implement list of proof types with some infos

--- src/views/proofTypes/ProofTypes.jsx
+++ src/views/proofTypes/ProofTypes.jsx
@@ -4,13 +4,46 @@ import { drizzleConnect } from 'drizzle-react';
 import { useTranslation } from 'react-i18next';
 import Container from '../../components/Container';
 import PropTypes from 'prop-types';
+import { Divider } from '@material-ui/core';
 
 function ProofTypes(props, context) {
 	const { t } = useTranslation();
 
 	return (
 		<Container>
-			<Box title="Proof types"></Box>
+			<Box title="Proof types">
+				<div style={{ fontFamily: 'arial' }}>
+					{Object.keys(props.proofTypes).map((addr, index) => {
+						let proofType = props.proofTypes[addr];
+						let name = proofType.label;
+						let address = proofType.value;
+						// TODO used in x tokens...
+						return (
+							<span key={'proof_' + index}>
+								<b>{name}</b>
+								<br />
+								<a
+									style={{ fontSize: 'x-small' }}
+									href={'https://rinkeby.etherscan.io/address/' + address}
+									target="_blank">
+									{address}
+								</a>
+								<br />
+								{proofType.description}
+								<br />
+								{proofType.paramsEncoded && (
+									<small style={{ color: 'gray' }}>
+										<b>Parameters</b>: {proofType.paramsEncoded}
+									</small>
+								)}
+								{index < Object.keys(props.proofTypes).length - 1 && (
+									<Divider style={{ margin: '10px 0' }} variant="middle" />
+								)}
+							</span>
+						);
+					})}
+				</div>
+			</Box>
 		</Container>
 	);
 }
