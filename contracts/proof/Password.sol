0a44afb636c7edb44fc1c74d40d5555eccfc491a
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-17 15:25:01 +0100

    Update wording and TODOs in Password proof contract

--- contracts/proof/Password.sol
+++ contracts/proof/Password.sol
@@ -8,19 +8,19 @@ contract Password is Fin4BaseProofType {
     Fin4BaseProofType(Fin4MessagingAddress)
     public {
       name = "Password";
-      description = "A numeric password (PIN), which the token creator set, needs to be provided.";
+      description = "Approval if the user provides the password matching the one the token creator set.";
     }
 
-    // TODO support string parameters to be able to use a string-password here
     function submitProof_Password(address tokenAddrToReceiveProof, uint claimId, string memory password) public returns(bool) {
       // via https://ethereum.stackexchange.com/a/30914
       if (keccak256(abi.encodePacked((password))) == keccak256(abi.encodePacked((_getPassword(tokenAddrToReceiveProof))))) {
         _sendApproval(address(this), tokenAddrToReceiveProof, claimId);
       } else {
         string memory message = string(abi.encodePacked(
-          Fin4TokenStub(tokenAddrToReceiveProof).name(),
-          ": The password you provided is not matching the one set by the token creator."
-        ));
+              "Your claim on token \'",
+              Fin4TokenStub(tokenAddrToReceiveProof).name(),
+              "\' got rejected from proof type \'Password\' because the password you",
+              " provided does not match the one set by the token creator"));
         Fin4Messaging(Fin4MessagingAddress).addInfoMessage(address(this), msg.sender, message);
       }
       return true;
@@ -38,7 +38,9 @@ contract Password is Fin4BaseProofType {
     }
 
     function _getPassword(address token) private view returns(string memory) {
-      return tokenToParameter[token]; // TODO this must not be visible to someone using truffle console or similar!
+      // TODO this must not be visible to someone using truffle console or similar!
+      // Use a salt additionally? Or encrypt it with itself? Only possible with numbers (as done in TCR?) #ConceptualDecision
+      return tokenToParameter[token];
     }
 
 }
