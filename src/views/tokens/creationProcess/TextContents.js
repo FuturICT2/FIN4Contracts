7f7763efa75bdabf646c640efe14e6874140ece5
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-17 17:55:09 +0100

    Fix missing closing tag in TextContent

--- src/views/tokens/creationProcess/TextContents.js
+++ src/views/tokens/creationProcess/TextContents.js
@@ -5,15 +5,15 @@ const steps = ['Basics', 'Traits', 'Actions', 'Value', 'Proofs']; // Disbursemen
 const getStepContent = stepIndex => {
 	switch (stepIndex) {
 		case 0:
-			return 'Token name';
+			return 'Token name'; // Formerly: Basic infos
 		case 1:
-			return 'Token properties';
+			return 'Token properties'; // Formerly: Fundamental properties
 		case 2:
-			return 'Actions';
+			return 'Actions'; // Formerly: For what action(s) can people claim this token?
 		case 3:
-			return 'Positive actions';
+			return 'Positive actions'; // Formerly: What quantity can be obtained per claim?
 		case 4:
-			return 'Proofs of good work';
+			return 'Proofs of good work'; // Formerly: Add proof types that users will have to provide
 		default:
 			return '';
 	}
@@ -22,7 +22,7 @@ const getStepContent = stepIndex => {
 const getStepInfoBoxContent = stepIndex => {
 	switch (stepIndex) {
 		case 0: // Basics
-			return (		
+			return (
 				<>
 					<b>Name</b>
 					<br />
@@ -31,14 +31,15 @@ const getStepInfoBoxContent = stepIndex => {
 					<br />
 					<b>Symbol</b>
 					<br />
-					Choose a short symbol of 3-5 letters (numbers are allowd, too) for your new token. Please note, that the system rejects symbols that are already in use.
+					Choose a short symbol of 3-5 letters (numbers are allowd, too) for your new token. Please note, that the
+					system rejects symbols that are already in use.
 					<br />
 					<br />
 					<b>Description</b>
 					<br />
 					Describe the purpose/idea of your new token in a short and understandable text.
 				</>
-			);				
+			);
 		case 1: // Traits (= Properties)
 			return (
 				<>
@@ -72,35 +73,41 @@ const getStepInfoBoxContent = stepIndex => {
 					<br />
 					<b>Decimals</b>
 					<br />
-					The digits by which your token is divisible. Zero means that users can only have natural numbers (0,1,2,3,4..) as balance on
-					your token and only amounts in natural numbers can be transferred. Other number indicate the decimal places, e.g., "3" means the token is divisible with 0.001 being the smallest unit.
+					The digits by which your token is divisible. Zero means that users can only have natural numbers (0,1,2,3,4..)
+					as balance on your token and only amounts in natural numbers can be transferred. Other number indicate the
+					decimal places, e.g., "3" means the token is divisible with 0.001 being the smallest unit.
 					<br />
 					<br />
 					<b>Initial token supply</b>
 					<br />
-					As the token creator, you can give yourself an initial amount of your new token. The total supply (above) will adjust to this amount.				</>
+					As the token creator, you can give yourself an initial amount of your new token. The total supply (above) will
+					adjust to this amount.{' '}
+				</>
 			);
 		case 2: // Actions
 			return (
 				<>
 					<b>Positive actions</b>
 					<br />
-					For what positive actions should users be able to obtain your new token? Take your time to think about this question. 
-					It very important for the future success for your token idea in the system. Also, it is important that users 
-					are able to prove they did the actions using the different proving methods (c.f. last section).
-			);			
-		case 3: // Value 		
+					For what positive actions should users be able to obtain your new token? Take your time to think about this
+					question. It very important for the future success for your token idea in the system. Also, it is important
+					that users are able to prove they did the actions using the different proving methods (c.f. last section).
+				</>
+			);
+		case 3: // Value
 			return (
 				<>
 					<b>Fixed amount</b>
 					<br />
-					Once the claim is successful, this fixed amount of tokens will be minted to the user. Default is 1 token per action.
+					Once the claim is successful, this fixed amount of tokens will be minted to the user. Default is 1 token per
+					action.
 					<br />
 					<br />
 					<b>Fixed factor</b>
 					<br />
-					The user can put a quantity as part of their claim (e.g., "I claim 3 tokens for that action"). Once the claim is successful,
-					the amount minted to the user is that quantity multiplied with the fixed factor you set here. Default is a fixed factor of 1.
+					The user can put a quantity as part of their claim (e.g., "I claim 3 tokens for that action"). Once the claim
+					is successful, the amount minted to the user is that quantity multiplied with the fixed factor you set here.
+					Default is a fixed factor of 1.
 				</>
 			);
 		case 4: // Proofs
@@ -108,14 +115,15 @@ const getStepInfoBoxContent = stepIndex => {
 				<>
 					<b>Proving actions (read thoroughly)</b>
 					<br />
-					To obtain tokens, users need to prove to the system that they actually performed the action required. You can choose any combination
-					of proofs from the list. Please take your time to think precisely about any combination you choose. Good proofs are suitable to the
-					the nature of the token, suitable and practical for the users trying to obtain them, and practical for the token creators. The harder
-					the proving is, the less users will try to obtain your token; the easier the proving is, the less perceived quality users will see in 
-					the token. In complex cases, you may want to experiment with different token designs at the same time.
-					Finally, proving actions is a complex matter and we constantly work to improve the proving mechanisms.
+					To obtain tokens, users need to prove to the system that they actually performed the action required. You can
+					choose any combination of proofs from the list. Please take your time to think precisely about any combination
+					you choose. Good proofs are suitable to the the nature of the token, suitable and practical for the users
+					trying to obtain them, and practical for the token creators. The harder the proving is, the less users will
+					try to obtain your token; the easier the proving is, the less perceived quality users will see in the token.
+					In complex cases, you may want to experiment with different token designs at the same time. Finally, proving
+					actions is a complex matter and we constantly work to improve the proving mechanisms.
 					<br />
-					<br />				
+					<br />
 					<b>Proof type: picture</b>
 					<br />
 					The claimer submits a picture, based on which the approver will decide on the claim.
@@ -128,10 +136,11 @@ const getStepInfoBoxContent = stepIndex => {
 					<br />
 					<b>Proof type: selfie-together</b>
 					<br />
-					The claimer submits a picture, based on which another approver and a member of a group of users appointed by the token creator decide to approve.
-					Put simpler: If the token requires another person to be involved (e.g. for a service), they need to approve, too. 
+					The claimer submits a picture, based on which another approver and a member of a group of users appointed by
+					the token creator decide to approve. Put simpler: If the token requires another person to be involved (e.g.
+					for a service), they need to approve, too.
 					<br />
-					<br />				
+					<br />
 					<b>Proof type: specific address</b>
 					<br />
 					The claimer has to specify an address, which has to approve.
@@ -141,7 +150,7 @@ const getStepInfoBoxContent = stepIndex => {
 					<br />
 					The token creator has to approve. (Short cut for specific address = token creator address.)
 					<br />
-					<br />				
+					<br />
 					<b>Proof type: password</b>
 					<br />
 					The claimer has to provide a numeric password (PIN) you as token creator define. Handle with care!
@@ -151,20 +160,20 @@ const getStepInfoBoxContent = stepIndex => {
 					<br />
 					The claimer can approve their own claims. Handle with care!
 					<br />
-					<br />				
+					<br />
 					<b>Proof type: minimum interval</b>
 					<br />
 					Defines a minimum time that has to pass between claims by same user. Handle with care!
 					<br />
-					<br />	
+					<br />
 					<b>Proof type: maximum quantity per interval</b>
 					<br />
 					Defines the maximum quantity a user can claim within a specified time interval. Handle with care!
 					<br />
-					<br />	
+					<br />
 					<b>Proof type: group member approval</b>
 					<br />
-					The token creator specifies one or more user groups, of which one member has to approve. Handle with care!			
+					The token creator specifies one or more user groups, of which one member has to approve. Handle with care!
 				</>
 			);
 		default:
