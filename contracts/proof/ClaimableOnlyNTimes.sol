8017d175039bd50e6fde2f48061f37ef48ceaf63
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-17 15:19:12 +0100

    Implement submitProof_ClaimableOnlyNTimes()

--- contracts/proof/ClaimableOnlyNTimes.sol
+++ contracts/proof/ClaimableOnlyNTimes.sol
@@ -7,12 +7,24 @@ contract ClaimableOnlyNTimes is Fin4BaseProofType {
     constructor(address Fin4MessagingAddress)
     Fin4BaseProofType(Fin4MessagingAddress)
     public {
-      name = "ClaimableOnlyNTimes";
-      description = "The token creator sets a cap how many times a token can be claimed";
+        name = "ClaimableOnlyNTimes";
+        description = "The token creator sets a cap how many times a token can be claimed";
     }
 
   function submitProof_ClaimableOnlyNTimes(address tokenAddrToReceiveProof, uint claimId) public returns(bool) {
-      // TODO
+      uint usersClaimCountOnToken = userToTheirClaimsCountOnToken[msg.sender][tokenAddrToReceiveProof];
+      uint cap = tokenToClaimsCap[tokenAddrToReceiveProof];
+      if (usersClaimCountOnToken == cap) {
+          string memory message = string(abi.encodePacked(
+              "Your claim on token \'",
+              Fin4TokenStub(tokenAddrToReceiveProof).name(),
+              "\' got rejected from proof type \'ClaimableOnlyNTimes\' because you reached the",
+              " maximum number of claims as capped by the token creator: ", uint2str(cap)));
+          Fin4Messaging(Fin4MessagingAddress).addInfoMessage(address(this), msg.sender, message);
+      } else {
+          _sendApproval(address(this), tokenAddrToReceiveProof, claimId);
+          userToTheirClaimsCountOnToken[msg.sender][tokenAddrToReceiveProof] = usersClaimCountOnToken + 1;
+      }
   }
 
   // @Override
