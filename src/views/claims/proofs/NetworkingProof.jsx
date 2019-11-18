1a835faf4a45f31a960bb19af39d6da49828c7db
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-18 22:39:48 +0100

    Show green checkmark if proof submitted

--- src/views/claims/proofs/NetworkingProof.jsx
+++ src/views/claims/proofs/NetworkingProof.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { drizzleConnect } from 'drizzle-react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
@@ -6,6 +6,9 @@ import AddressQRreader from '../../../components/AddressQRreader';
 import { TextField } from '@material-ui/core';
 import { isValidPublicAddress } from '../../../components/Contractor';
 import Button from '../../../components/Button';
+import CheckIcon from '@material-ui/icons/CheckCircle';
+import { IconButton } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 function NetworkingProof(props, context) {
 	const { t } = useTranslation();
@@ -22,29 +25,45 @@ function NetworkingProof(props, context) {
 			.send({ from: props.store.getState().fin4Store.defaultAccount })
 			.then(result => {
 				console.log('Results of submitting Networking.submitProof: ', result);
+				setProofSubmitted(true);
 			});
 	};
 
 	const addressValue = useRef(null);
 	const contentValue = useRef('');
+	const [proofSubmitted, setProofSubmitted] = useState(false);
 
 	return (
 		<>
-			<AddressQRreader onChange={val => (addressValue.current = val)} label="Public address of approver" />
-			<br />
-			<div style={{ fontFamily: 'arial', color: 'gray' }}>Snippets from your interaction:</div>
-			<br />
-			<TextField
-				multiline
-				rows="2"
-				fullWidth
-				variant="outlined"
-				onChange={e => (contentValue.current = e.target.value)}
-			/>
-			<br />
-			<Button onClick={submit} center="true">
-				Submit
-			</Button>
+			{proofSubmitted ? (
+				<center>
+					<Typography>Proof submitted</Typography>
+					<br />
+					<IconButton style={{ color: 'green', transform: 'scale(2.4)' }}>
+						<CheckIcon />
+					</IconButton>
+					<br />
+					<br />
+				</center>
+			) : (
+				<>
+					<AddressQRreader onChange={val => (addressValue.current = val)} label="Public address of approver" />
+					<br />
+					<div style={{ fontFamily: 'arial', color: 'gray' }}>Snippets from your interaction:</div>
+					<br />
+					<TextField
+						multiline
+						rows="2"
+						fullWidth
+						variant="outlined"
+						onChange={e => (contentValue.current = e.target.value)}
+					/>
+					<br />
+					<Button onClick={submit} center="true">
+						Submit
+					</Button>
+				</>
+			)}
 		</>
 	);
 }
