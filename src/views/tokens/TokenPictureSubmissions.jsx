91df9e7e76012a4fe96c6c955a9625a7f71ff541
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-19 00:02:50 +0100

    Styling of picture submissions page

--- src/views/tokens/TokenPictureSubmissions.jsx
+++ src/views/tokens/TokenPictureSubmissions.jsx
@@ -32,14 +32,23 @@ function TokenPictureSubmissions(props) {
 		<div style={{ fontFamily: 'arial', padding: '0 50px 0 50px' }}>
 			{getSubmissionsOnToken().map((sub, idx) => {
 				let date = moment.unix(sub.timestamp).calendar();
+				let imgURL = 'https://gateway.ipfs.io/ipfs/' + sub.content;
+				// TODO use a collage tool to make this properly?
 				return (
 					<span key={'content_' + idx}>
-						<span
-							title={'By ' + sub.user + ', ' + date} // TODO add more fields here?
-						>
-							{sub.content}
-						</span>
-						&nbsp;&nbsp;
+						<a href={imgURL} target="_blank">
+							<img
+								src={imgURL}
+								style={{
+									maxWidth: '300px',
+									maxHeight: '200px',
+									borderRadius: '20px',
+									// boxShadow values copied from www.w3schools.com/css/css3_shadows.asp
+									boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+								}}
+								title={'By ' + sub.user + ', ' + date}></img>
+						</a>
+						&nbsp;&nbsp;&nbsp;&nbsp;
 					</span>
 				);
 			})}
