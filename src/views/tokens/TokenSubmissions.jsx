87a6f5e0bc95c6c7fa3c140df830e55206f7bf57
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-11-18 18:09:49 +0100

    Style submissions to vary in front color and size

--- src/views/tokens/TokenSubmissions.jsx
+++ src/views/tokens/TokenSubmissions.jsx
@@ -28,13 +28,39 @@ function TokenSubmissions(props) {
 			.filter(sub => sub.token === token.address);
 	};
 
+	/*const getRandomColor = () => { // from stackoverflow.com/a/1484514/2474159
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i ++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };*/
+
+	const getRandomFin4Color = () => {
+		// first 4 taken from the Finance 4.0 logo, last 2 are main and dark from colors-config.json
+		let colors = ['#00a3f0', '#6a5fad', '#cd1d70', '#3d363e', '#84aede', '#242e5e'];
+		return colors[Math.floor(Math.random() * colors.length)];
+	};
+
+	const getRandomFontSize = () => {
+		// from stackoverflow.com/a/45791988/2474159
+		let min = 15;
+		let maxAddToMin = 20;
+		return Math.floor(Math.random() * maxAddToMin + min) + 'px';
+	};
+
 	return (
-		<>
+		<div style={{ fontFamily: 'arial', padding: '0 50px 0 50px' }}>
 			{getSubmissionsOnToken().map(sub => {
-				// TODO
-				return sub.content;
+				return (
+					<>
+						<span style={{ color: getRandomFin4Color(), fontSize: getRandomFontSize() }}>{sub.content}</span>
+						&nbsp;&nbsp;
+					</>
+				);
 			})}
-		</>
+		</div>
 	);
 }
 
