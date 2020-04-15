5c1d704b3d3a24fd834655a2d6f0c5cefc67e98b
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2020-04-15 10:14:54 +0200

    Upgrade to openzeppelin 2.5.0

--- contracts/tokens/ERC20Plus.sol
+++ contracts/tokens/ERC20Plus.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
 
 /**
