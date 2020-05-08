e5191116c734f1d17ac30fac0c1b85c7baae3ba6
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2020-05-08 11:44:56 +0200

    Implement setParameters and successfulClaimCallback on MintingUnderlying

--- contracts/underlyings/MintingUnderlying.sol
+++ contracts/underlyings/MintingUnderlying.sol
@@ -1,17 +1,57 @@
 pragma solidity ^0.5.17;
 
 import 'contracts/underlyings/UnderlyingInterface.sol';
+import 'contracts/stub/MintingStub.sol';
 
 contract MintingUnderlying is UnderlyingInterface {
 
-    function successfulClaimCallback(address tokenAddress, address claimer, uint quantity) public {
-        // TODO
+    function successfulClaimCallback(address hostTokenForUnderlying, address claimer, uint quantity) public {
+        // msg.sender must be Fin4Underlying, plus other security checks?
+        ParameterSet storage parameterSet = tokenToParameterSet[hostTokenForUnderlying];
+
+        address beneficiary;
+        if (parameterSet.beneficiary == address(0)) {
+            beneficiary = claimer;
+        } else {
+            beneficiary = parameterSet.beneficiary;
+        }
+
+        uint amount;
+        if (parameterSet.fixedAmount == 0) {
+            amount = quantity * parameterSet.quantityFactor;
+        } else {
+            amount = parameterSet.fixedAmount;
+        }
+
+        MintingStub(parameterSet.tokenToMint).mint(beneficiary, amount);
     }
 
     // if beneficiary is the zero address, mint to the claimer
     function getParameterForTokenCreatorToSetEncoded() public pure returns(string memory) {
         return "address:tokenToMint:address of token to mint,address:beneficiary:leave blank for it to be the claimer,"
             "uint:fixedAmount:per successful claim:int,uint:quantityFactor:claimed quantity gets multiplied by this";
+    }
+
+    struct ParameterSet {
+        address tokenCreator;
+        address hostTokenForUnderlying;
+        address tokenToMint;
+        address beneficiary;
+        uint fixedAmount;
+        uint quantityFactor;
+    }
+
+    mapping (address => ParameterSet) public tokenToParameterSet;
+
+    function setParameters(address hostTokenForUnderlying, address tokenToMint, address beneficiary,
+        uint fixedAmount, uint quantityFactor) public {
+        ParameterSet storage parameterSet = tokenToParameterSet[hostTokenForUnderlying];
+        parameterSet.tokenCreator = msg.sender;
+        parameterSet.hostTokenForUnderlying = hostTokenForUnderlying;
+        parameterSet.tokenToMint = tokenToMint;
+        parameterSet.beneficiary = beneficiary;
+        parameterSet.fixedAmount = fixedAmount;
+        parameterSet.quantityFactor = quantityFactor;
     }
 
 }
