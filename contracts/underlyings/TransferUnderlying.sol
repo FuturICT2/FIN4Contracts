eedf4bde8e54fd347c66a3bcaea614441fa72205
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2020-05-10 21:12:46 +0200

    Enable Ether to be received and send from TransferUnderlying

--- contracts/underlyings/TransferUnderlying.sol
+++ contracts/underlyings/TransferUnderlying.sol
@@ -6,6 +6,13 @@ import 'contracts/stub/MintTransferStub.sol';
 
 contract TransferUnderlying is UnderlyingInterface, UnderlyingParameterizedInterface {
 
+    uint public ethBalance = 0;
+
+    // seems to work on testnet, but not on Ganache
+    function() external payable { // enables sending ether to this contract
+        ethBalance += msg.value;
+    }
+
     function successfulClaimCallback(address hostTokenForUnderlying, address claimer, uint quantity) public {
         // msg.sender must be Fin4Underlying, plus other security checks?
         ParameterSet storage parameterSet = tokenToParameterSet[hostTokenForUnderlying];
@@ -24,13 +31,20 @@ contract TransferUnderlying is UnderlyingInterface, UnderlyingParameterizedInter
             amount = parameterSet.fixedAmount;
         }
 
-        MintTransferStub(parameterSet.tokenToTransfer).transfer(beneficiary, amount);
+        if (parameterSet.tokenToTransfer == address(0)) {
+            ethBalance -= amount;
+            // in wei, see https://solidity.readthedocs.io/en/v0.5.15/units-and-global-variables.html?highlight=ether#ether-units
+            address payable beneficiaryPayble = address(uint160(beneficiary)); // via https://ethereum.stackexchange.com/a/64109/56047
+            beneficiaryPayble.transfer(amount);
+        } else {
+            MintTransferStub(parameterSet.tokenToTransfer).transfer(beneficiary, amount);
+        }
     }
 
     // if beneficiary is the zero address, transfer to the claimer
     // requires this contract to have a balance on this token TODO think about how to check/ensure that, edge cases etc.
     function getParameterForTokenCreatorToSetEncoded() public pure returns(string memory) {
-        return "address:tokenToTransfer:address of token to transfer,address:beneficiary:leave blank for it to be the claimer,"
+        return "address:tokenToTransfer:leave blank for it to be Ether,address:beneficiary:leave blank for it to be the claimer,"
             "uint:fixedAmount:per successful claim:int,uint:quantityFactor:multiply claimed quantity";
     }
 
