301dae84ab263614f68b05f40828610fb942eed4
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-05-21 10:23:33 +0200

    Add isSourcerer flag to Underlying struct

--- contracts/Fin4Underlyings.sol
+++ contracts/Fin4Underlyings.sol
@@ -13,6 +13,7 @@ contract Fin4Underlyings {
     struct Underlying {
         uint id;
         bytes32 name;
+        bool isSourcerer;
         address contractAddress; // can be external to Fin4, must implement xyz
         // TODO: valid for / editable later on / what else?
         // TODO a visibility boolean to optionally hide it in frontend?
@@ -20,29 +21,32 @@ contract Fin4Underlyings {
 
     uint public nextUnderlyingId = 0;
 
-    function addUnderlying(bytes32 name, address contractAddress) public {
+    function addUnderlying(bytes32 name, address contractAddress, bool isSourcerer) public {
         Underlying storage underlying = underlyings[nextUnderlyingId];
         underlying.id = nextUnderlyingId;
         underlying.name = name;
+        underlying.isSourcerer = isSourcerer;
         underlying.contractAddress = contractAddress;
         nextUnderlyingId ++;
     }
 
     function addSourcerer(bytes32 name, address contractAddress) public {
         sourcererAddresses.push(contractAddress);
-        addUnderlying(name, contractAddress);
+        addUnderlying(name, contractAddress, true);
     }
 
-    function getUnderlyings() public view returns(uint[] memory, bytes32[] memory, address[] memory) {
+    function getUnderlyings() public view returns(uint[] memory, bytes32[] memory, bool[] memory, address[] memory) {
         uint[] memory ids = new uint[](nextUnderlyingId);
         bytes32[] memory names = new bytes32[](nextUnderlyingId);
+        bool[] memory isSourcerers = new bool[](nextUnderlyingId);
         address[] memory contractAddresses = new address[](nextUnderlyingId);
         for (uint i = 0; i < nextUnderlyingId; i ++) {
             ids[i] = underlyings[i].id;
             names[i] = underlyings[i].name;
+            isSourcerers[i] = underlyings[i].isSourcerer;
             contractAddresses[i] = underlyings[i].contractAddress;
         }
-        return (ids, names, contractAddresses);
+        return (ids, names, isSourcerers, contractAddresses);
     }
 
     function getParamsEncodedOnUnderlyingContract(address contractAddress) public view returns(string memory) {
