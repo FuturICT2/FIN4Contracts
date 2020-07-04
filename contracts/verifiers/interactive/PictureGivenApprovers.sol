bd6deb3f51278a4e861e672b1c742298517c71f1
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-07-04 22:59:13 +0200

    Wire up PictureGivenApprovers to use super method

--- contracts/verifiers/interactive/PictureGivenApprovers.sol
+++ contracts/verifiers/interactive/PictureGivenApprovers.sol
@@ -4,12 +4,14 @@ import "contracts/verifiers/interactive/ApprovalByUsersOrGroups.sol";
 
 contract PictureGivenApprovers is ApprovalByUsersOrGroups {
 
-    constructor() public {}
-
     // @Override
     function init() public {
         name = "sc.verifier.picture-given-approvers.name";
         description = "sc.verifier.picture-given-approvers.description";
     }
 
+    function submitProof_PictureGivenApprovers(address tokenAddrToReceiveVerifierNotice, uint claimId, string memory IPFShash) public {
+        submitProof(msg.sender, tokenAddrToReceiveVerifierNotice, claimId, IPFShash,
+            "The appointed approvers have been notified about your picture approval request.");
+    }
 }
